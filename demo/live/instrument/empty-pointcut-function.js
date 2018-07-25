33b48b83996b5612878f27543cb13ce12b9380eb
lachrist <lachrist@vub.ac.be>
2018-07-25 18:50:28 +0200

    corrected a bug where pointcut's function did receive node but the last trap's argument instead

---
+++ demo/live/instrument/empty-pointcut-function.js
@@ -0,0 +1,9 @@
+const Acorn = require("acorn");
+const Aran = require("aran");
+global.ADVICE = {};
+const aran = Aran({namespace:"ADVICE", output:"String"});
+global.eval(aran.setup());
+module.exports = (script) => aran.weave(Acorn.parse(script), (name, node) => {
+  console.log(name, node.AranSerial, node.type);
+  return false;
+});
