e1472f3ee47ab0e157c7af836c16752b5cac420d
Laurent Christophe <lachrist@vub.ac.be>
2018-09-05 15:23:15 +0200

    demo

--- demo/dead/spawn.js
+++ demo/dead/spawn.js
@@ -2,16 +2,18 @@ const Aran = require("aran");
 const Acorn = require("acorn");
 const Astring = require("astring");
 module.exports = (pointcut, advice) => {
-  const aran = Aran({
-    namespace: "META",
-    pointcut: eval(pointcut)
-  });
+  const aran = Aran({namespace:"META"});
+  pointcut = eval(pointcut);
   return (path, script, argv) => {
-    const estree = Acorn.parse(script);
+    const estree1 = Acorn.parse(script);
+    const estree2 = aran.weave(estree1, pointcut);
     return new Worker(URL.createObjectURL(new Blob([
+      "console.log = function () { \n",
+      "  postMessage(Array.from(arguments).map(String).join(' ')+'\\n');\n",
+      "};\n",
       "var META = "+advice+";\n",
-      Astring.generate(aran.setup())+"\n",
-      Astring.generate(aran.weave(estree))
+      "{\n"+Astring.generate(aran.setup(pointcut))+"\n}\n",
+      "{\n"+Astring.generate(estree2)+"\n}"
     ])));
   };
 };
