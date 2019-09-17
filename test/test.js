4e67198c07c71c6451641fa6221902531f14137e
Laurent Christophe <lachrist@vub.ac.be>
2019-09-17 16:41:44 +0200

    corrected bug where variables (var) defined in else branch would not be hoisted

--- test/test.js
+++ test/test.js
@@ -1,18 +1,20 @@
 const Aran = require("aran");
 const Acorn = require("acorn");
 const Astring = require("astring");
-const aran = Aran({format:"estree"});
-
+const format = "estree";
+const aran = Aran({format});
+const generate = format === "estree" ? (estree) => Astring.generate(estree) : (script) => script;
 const advice = {__proto__:null};
-const script1 = `(function () {
-  switch ("foo") {
-    case "foo": true || false;
+const script1 = `(function() {
+  if (true) {
+  } else {
+    var xyz = 123;
   }
 });`;
 const estree1 = Acorn.parse(script1); 
 const estree2 = aran.weave(estree1, Object.keys(advice));
-const script2 = Astring.generate(estree2);
+const script2 = generate(estree2);
 console.log(script2);
 global[aran.namespace] = advice;
-global.eval(Astring.generate(aran.setup()));
+global.eval(generate(aran.setup()));
 global.eval(script2);
--- test/yo.js
+++ test/yo.js
@@ -1,1 +1,7 @@
-id();
+(function() {
+  if (computed || this.eat(types.dot)) {
+  } else if (!noCalls && this.eat(types.parenL)) {
+    var refDestructuringErrors = 1;
+  }
+  return base
+});
