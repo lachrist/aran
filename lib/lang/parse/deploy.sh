73cad5d0a1c3c6d76a488657f071a7316d232723
Laurent Christophe <lachrist@vub.ac.be>
2020-12-15 11:45:10 +0100

    parameters are now bundled into a single input

--- lib/lang/parse/deploy.sh
+++ lib/lang/parse/deploy.sh
@@ -1,4 +1,4 @@
-pegjs --trace --allowed-start-rules _,Label,Identifier,Declarable,String,Number,BigInt,StartProgram,StartPrelude,StartBlock,StartStatement,StartExpression parser.pegjs &&
+pegjs --trace --allowed-start-rules _,Label,Identifier,String,Number,BigInt,StartProgram,StartPrelude,StartBlock,StartStatement,StartExpression parser.pegjs &&
 node parser.test.js &&
 nyc --reporter=html --include parser.js node parser.test.js &&
 pegjs --allowed-start-rules StartProgram,StartPrelude,StartBlock,StartStatement,StartExpression parser.pegjs &&
