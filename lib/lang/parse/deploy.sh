f414eb84d830791bd41f021a3bd1590ed66ddde8
Laurent Christophe <lachrist@vub.ac.be>
2021-01-12 17:12:05 +0100

    label blocks to get rid of the closure wrapping programs + big rena,ing

--- lib/lang/parse/deploy.sh
+++ lib/lang/parse/deploy.sh
@@ -1,5 +1,5 @@
-pegjs --trace --allowed-start-rules _,Label,Identifier,String,Number,BigInt,StartProgram,StartPrelude,StartBlock,StartStatement,StartExpression parser.pegjs &&
+pegjs --trace --allowed-start-rules _,Label,Identifier,String,Number,BigInt,StartProgram,StartLink,StartBlock,StartLabelBlock,StartStatement,StartExpression parser.pegjs &&
 node parser.test.js &&
 nyc --reporter=html --include parser.js node parser.test.js &&
-pegjs --allowed-start-rules StartProgram,StartPrelude,StartBlock,StartStatement,StartExpression parser.pegjs &&
+pegjs --allowed-start-rules StartProgram,StartLink,StartLabelBlock,StartBlock,StartStatement,StartExpression parser.pegjs &&
 echo "done"
