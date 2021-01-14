f88d62d5d5df345434cb358aaf6d94fef6d53696
Laurent Christophe <lachrist@vub.ac.be>
2021-01-14 12:50:50 +0100

    slightly modified BundleStatement into ListStatement, which are now not automatically flaten on construction and exists a proper statement node, this means that we accept an ambiguous grammar but it admits a straighforward normal form

--- lib/lang/parse/deploy.sh
+++ lib/lang/parse/deploy.sh
@@ -1,5 +1,5 @@
-pegjs --trace --allowed-start-rules _,Label,Identifier,String,Number,BigInt,StartProgram,StartLink,StartBlock,StartLabelBlock,StartStatement,StartExpression parser.pegjs &&
+pegjs --trace --allowed-start-rules _,Label,Identifier,String,Number,BigInt,StartProgram,StartLink,StartBlock,StartBranch,StartSingleStatement,StartExpression parser.pegjs &&
 node parser.test.js &&
 nyc --reporter=html --include parser.js node parser.test.js &&
-pegjs --allowed-start-rules StartProgram,StartLink,StartLabelBlock,StartBlock,StartStatement,StartExpression parser.pegjs &&
+pegjs --allowed-start-rules StartProgram,StartLink,StartBranch,StartBlock,StartSingleStatement,StartExpression parser.pegjs &&
 echo "done"
