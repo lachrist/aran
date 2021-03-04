ae4caf834f1c43e9565a577b593100e469007523
Laurent Christophe <lachrist@vub.ac.be>
2021-03-04 17:10:28 +0100

    tested scope/meta

--- lib/lang/parse/deploy.sh
+++ lib/lang/parse/deploy.sh
@@ -1,5 +1,5 @@
-pegjs --trace --allowed-start-rules _,LabelIdentifier,VariableIdentifier,String,Number,BigInt,StartProgram,StartLink,StartBlock,StartBranch,StartSingleStatement,StartExpression parser.pegjs &&
+pegjs --trace --allowed-start-rules _,LabelIdentifier,VariableIdentifier,String,Number,BigInt,StartProgram,StartLink,StartBlock,StartBranch,StartSingleStatement,StartStatement,StartExpression parser.pegjs &&
 node parser.test.js &&
 nyc --reporter=html --include parser.js node parser.test.js &&
-pegjs --allowed-start-rules StartProgram,StartLink,StartBranch,StartBlock,StartSingleStatement,StartExpression parser.pegjs &&
+pegjs --allowed-start-rules StartProgram,StartLink,StartBranch,StartBlock,StartStatement,StartSingleStatement,StartExpression parser.pegjs &&
 echo "done"
