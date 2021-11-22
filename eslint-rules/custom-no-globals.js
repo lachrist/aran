47b274ccb6041a7dc40369ff877cf5b1b2a9faf2
Laurent Christophe <lachrist@vub.ac.be>
2021-11-22 12:27:21 +0100

    build: custome rules to disable globals
    The existing no-restricted-globals rule blacklist globals whereas I wanted to whitelist them.

---
+++ eslint-rules/custom-no-globals.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+  meta: {
+    type: "suggestion",
+    docs: {
+      description: "disallow global variables",
+      recommended: false,
+    },
+    schema: {
+      type: "array",
+      items: {type: "string"},
+    },
+  },
+  create: (context) => {
+    const {options:allowed} = context;
+    const reportReference = (reference) => {
+      const {identifier} = reference;
+      const {name} = identifier;
+      if (!allowed.includes(name)) {
+        context.report({
+          node: identifier,
+          message: `Forbidden global variable: ${name}`,
+        });
+      }
+    };
+    const reportVariable = ({references}) => {
+      references.forEach(reportReference);
+    };
+    const isVariableNotDefined = ({defs:{length}}) => length === 0;
+    return {
+      Program: () => {
+        const {variables, through} = context.getScope();
+        variables.filter(isVariableNotDefined).forEach(reportVariable);
+        // Duplicate with no-undef
+        // through.forEach(reportReference);
+      },
+    };
+  },
+};
