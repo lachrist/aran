e6ef38561ff7d49cfd6d246006b0f98c1b84aacc
Laurent Christophe <lachrist@vub.ac.be>
2022-05-08 21:37:14 +0200

    refactor: factor out dynamic scoping operations from base

---
+++ src/transpile/scope/dynamic.mjs
@@ -0,0 +1,227 @@
+import {
+  makeEffectStatement,
+  makeExpressionEffect,
+  makeConditionalExpression,
+  makeConditionalEffect,
+  makeLiteralExpression,
+} from "../../ast/index.mjs";
+
+import {
+  makeHasExpression,
+  makeGetExpression,
+  makeSetExpression,
+  makeDeleteExpression,
+  makeDefineExpression,
+  makeBinaryExpression,
+  makeSimpleObjectExpression,
+  makeDirectIntrinsicExpression,
+  makeThrowSyntaxErrorExpression,
+  makeThrowReferenceErrorExpression,
+} from "../intrinsic.mjs";
+
+const {undefined} = globalThis;
+
+const EMPTY = null;
+const RIGID = true;
+const LOOSE = false;
+
+const generateMakeDynamicFrame = (scoping) => (unscopable, frame) => ({
+  frame,
+  scoping,
+  unscopable,
+});
+
+export const makeEmptyDynamicFrame = generateMakeDynamicFrame(EMPTY);
+
+export const makeLooseDynamicFrame = generateMakeDynamicFrame(LOOSE);
+
+export const makeRigidDynamicFrame = generateMakeDynamicFrame(RIGID);
+
+/////////////
+// Prelude //
+/////////////
+
+export const makePreludeStatementArray = ({scoping, frame}, variable) =>
+  scoping === RIGID
+    ? [
+        makeEffectStatement(
+          makeExpressionEffect(
+            makeConditionalExpression(
+              makeHasExpression(frame, makeLiteralExpression(variable)),
+              makeThrowSyntaxErrorExpression(
+                `Identifier '${variable}' has already been declared`,
+              ),
+              makeLiteralExpression({undefined: null}),
+            ),
+          ),
+        ),
+      ]
+    : [];
+
+///////////
+// Loose //
+///////////
+
+export const makeLooseDeclareStatementArray = ({scoping, frame}, variable) =>
+  scoping === LOOSE
+    ? [
+        makeEffectStatement(
+          makeExpressionEffect(
+            makeConditionalExpression(
+              makeHasExpression(frame, makeLiteralExpression(variable)),
+              makeLiteralExpression({undefined: null}),
+              makeDefineExpression(
+                frame,
+                makeLiteralExpression(variable),
+                makeSimpleObjectExpression(makeLiteralExpression(null), {
+                  configurable: makeLiteralExpression(false),
+                  enumerable: makeLiteralExpression(true),
+                  value: makeLiteralExpression({undefined: null}),
+                  writable: makeLiteralExpression(true),
+                }),
+              ),
+            ),
+          ),
+        ),
+      ]
+    : [];
+
+///////////
+// Rigid //
+///////////
+
+export const makeRigidDeclareStatementArray = ({scoping, frame}, variable) =>
+  scoping === RIGID
+    ? [
+        makeEffectStatement(
+          makeExpressionEffect(
+            makeDefineExpression(
+              frame,
+              makeLiteralExpression(variable),
+              makeSimpleObjectExpression(makeLiteralExpression(null), {
+                configurable: makeLiteralExpression(false),
+                enumerable: makeLiteralExpression(true),
+                value: makeDirectIntrinsicExpression("aran.deadzone"),
+                writable: makeLiteralExpression(true),
+              }),
+            ),
+          ),
+        ),
+      ]
+    : [];
+
+export const makeRigidInitializeStatementArray = (
+  {scoping, frame},
+  variable,
+  writable,
+  expression,
+) =>
+  scoping === RIGID
+    ? [
+        makeEffectStatement(
+          makeExpressionEffect(
+            makeDefineExpression(
+              frame,
+              makeLiteralExpression(variable),
+              makeSimpleObjectExpression(makeLiteralExpression(null), {
+                configurable: makeLiteralExpression(false),
+                enumerable: makeLiteralExpression(true),
+                value: expression,
+                writable: makeLiteralExpression(writable),
+              }),
+            ),
+          ),
+        ),
+      ]
+    : [];
+
+////////////
+// Lookup //
+////////////
+
+export const READ = null;
+export const DELETE = undefined;
+
+const makeScopableExpression = (unscopable, frame, variable) =>
+  unscopable
+    ? makeConditionalExpression(
+        makeGetExpression(
+          frame,
+          makeDirectIntrinsicExpression("Symbol.unscopables"),
+        ),
+        makeConditionalExpression(
+          makeGetExpression(
+            makeGetExpression(
+              frame,
+              makeDirectIntrinsicExpression("Symbol.unscopables"),
+            ),
+            makeLiteralExpression(variable),
+          ),
+          makeLiteralExpression(false),
+          makeHasExpression(frame, makeLiteralExpression(variable)),
+        ),
+        makeHasExpression(frame, makeLiteralExpression(variable)),
+      )
+    : makeHasExpression(frame, makeLiteralExpression(variable));
+
+const makeDeadzoneExpression = (scoping, frame, variable, alternate) =>
+  scoping === RIGID
+    ? makeConditionalExpression(
+        makeBinaryExpression(
+          "===",
+          makeGetExpression(frame, makeLiteralExpression(variable)),
+          makeDirectIntrinsicExpression("aran.deadzone"),
+        ),
+        makeThrowReferenceErrorExpression(
+          `Cannot access '${variable}' before initialization`,
+        ),
+        alternate,
+      )
+    : alternate;
+
+export const makeLookupNode = (
+  {scoping, unscopable, frame},
+  strict,
+  variable,
+  right,
+  next,
+) => {
+  if (unscopable === null) {
+    return next;
+  } else if (right === READ) {
+    return makeConditionalExpression(
+      makeScopableExpression(unscopable, frame, variable),
+      makeDeadzoneExpression(
+        scoping,
+        frame,
+        variable,
+        makeGetExpression(frame, makeLiteralExpression(variable)),
+      ),
+      next,
+    );
+  } else if (right === DELETE) {
+    return makeConditionalExpression(
+      makeScopableExpression(unscopable, frame, variable),
+      makeDeleteExpression(strict, frame, makeLiteralExpression(variable)),
+      next,
+    );
+  } else {
+    return makeConditionalEffect(
+      makeScopableExpression(unscopable, frame, variable),
+      makeExpressionEffect(
+        makeDeadzoneExpression(
+          scoping,
+          frame,
+          variable,
+          makeSetExpression(
+            strict || scoping === RIGID,
+            frame,
+            makeLiteralExpression(variable),
+            right,
+          ),
+        ),
+      ),
+      next,
+    );
+  }
+};
---
+++ src/transpile/scope/dynamic.test.mjs
@@ -0,0 +1,417 @@
+import {assertEqual} from "../../__fixture__.mjs";
+
+import {
+  makeBlock,
+  makeLiteralExpression,
+  makeExpressionEffect,
+} from "../../ast/index.mjs";
+
+import {
+  allignBlock,
+  allignExpression,
+  allignEffect,
+} from "../../allign/index.mjs";
+
+import {
+  READ,
+  DELETE,
+  // makeEmptyDynamicFrame,
+  makeLooseDynamicFrame,
+  makeRigidDynamicFrame,
+  makePreludeStatementArray,
+  makeLooseDeclareStatementArray,
+  makeRigidDeclareStatementArray,
+  makeRigidInitializeStatementArray,
+  makeLookupNode,
+} from "./dynamic.mjs";
+
+/////////////
+// Prelude //
+/////////////
+
+// loose prelude //
+assertEqual(
+  allignBlock(
+    makeBlock(
+      [],
+      [],
+      makePreludeStatementArray(
+        makeLooseDynamicFrame(null, makeLiteralExpression("frame")),
+        "variable",
+      ),
+    ),
+    "{}",
+  ),
+  null,
+);
+
+// rigid prelude //
+assertEqual(
+  allignBlock(
+    makeBlock(
+      [],
+      [],
+      makePreludeStatementArray(
+        makeRigidDynamicFrame(null, makeLiteralExpression("frame")),
+        "variable",
+      ),
+    ),
+    `
+      {
+        effect(
+          (
+            intrinsic('aran.has')(
+              undefined,
+              'frame',
+              'variable',
+            ) ?
+            intrinsic('aran.throw')(
+              undefined,
+              new (intrinsic('SyntaxError'))(
+                'Identifier \\'variable\\' has already been declared',
+              ),
+            ) :
+            undefined
+          ),
+        );
+      }
+    `,
+  ),
+  null,
+);
+
+///////////
+// Loose //
+///////////
+
+// empty declaration //
+assertEqual(
+  allignBlock(
+    makeBlock(
+      [],
+      [],
+      makeLooseDeclareStatementArray(
+        makeRigidDynamicFrame(null, makeLiteralExpression("frame")),
+        "variable",
+      ),
+    ),
+    "{}",
+  ),
+  null,
+);
+
+// loose declaration //
+assertEqual(
+  allignBlock(
+    makeBlock(
+      [],
+      [],
+      makeLooseDeclareStatementArray(
+        makeLooseDynamicFrame(null, makeLiteralExpression("frame")),
+        "variable",
+      ),
+    ),
+    `
+      {
+        effect(
+          (
+            intrinsic('aran.has')(
+              undefined,
+              'frame',
+              'variable'
+            ) ?
+            undefined :
+            intrinsic('Reflect.defineProperty')(
+              undefined,
+              'frame',
+              'variable',
+              intrinsic('aran.createObject')(
+                undefined,
+                null,
+                'configurable', false,
+                'enumerable', true,
+                'value', undefined,
+                'writable', true,
+              ),
+            )
+          ),
+        );
+      }
+    `,
+  ),
+  null,
+);
+
+///////////
+// Rigid //
+///////////
+
+// empty declaration //
+assertEqual(
+  allignBlock(
+    makeBlock(
+      [],
+      [],
+      makeRigidDeclareStatementArray(
+        makeLooseDynamicFrame(null, makeLiteralExpression("frame")),
+        "variable",
+      ),
+    ),
+    "{}",
+  ),
+  null,
+);
+
+// rigid declaration //
+assertEqual(
+  allignBlock(
+    makeBlock(
+      [],
+      [],
+      makeRigidDeclareStatementArray(
+        makeRigidDynamicFrame(null, makeLiteralExpression("frame")),
+        "variable",
+      ),
+    ),
+    `
+      {
+        effect(
+          intrinsic('Reflect.defineProperty')(
+            undefined,
+            'frame',
+            'variable',
+            intrinsic('aran.createObject')(
+              undefined,
+              null,
+              'configurable', false,
+              'enumerable', true,
+              'value', intrinsic('aran.deadzone'),
+              'writable', true,
+            ),
+          ),
+        );
+      }
+    `,
+  ),
+  null,
+);
+
+// empty initialization //
+assertEqual(
+  allignBlock(
+    makeBlock(
+      [],
+      [],
+      makeRigidInitializeStatementArray(
+        makeLooseDynamicFrame(null, makeLiteralExpression("frame")),
+        "variable",
+        true,
+        makeLiteralExpression("right"),
+      ),
+    ),
+    "{}",
+  ),
+  null,
+);
+
+// rigid initialization //
+assertEqual(
+  allignBlock(
+    makeBlock(
+      [],
+      [],
+      makeRigidInitializeStatementArray(
+        makeRigidDynamicFrame(null, makeLiteralExpression("frame")),
+        "variable",
+        false,
+        makeLiteralExpression("right"),
+      ),
+    ),
+    `
+      {
+        effect(
+          intrinsic('Reflect.defineProperty')(
+            undefined,
+            'frame',
+            'variable',
+            intrinsic('aran.createObject')(
+              undefined,
+              null,
+              'configurable', false,
+              'enumerable', true,
+              'value', 'right',
+              'writable', false,
+            ),
+          ),
+        );
+      }
+    `,
+  ),
+  null,
+);
+
+////////////
+// Lookup //
+////////////
+
+// read unlookupable //
+assertEqual(
+  allignExpression(
+    makeLookupNode(
+      makeLooseDynamicFrame(null, makeLiteralExpression("frame")),
+      false,
+      "variable",
+      READ,
+      makeLiteralExpression("next"),
+    ),
+    "'next'",
+  ),
+  null,
+);
+
+// read loose //
+assertEqual(
+  allignExpression(
+    makeLookupNode(
+      makeLooseDynamicFrame(false, makeLiteralExpression("frame")),
+      false,
+      "variable",
+      READ,
+      makeLiteralExpression("next"),
+    ),
+    `
+      (
+        intrinsic('aran.has')(undefined, 'frame', 'variable') ?
+        intrinsic('aran.get')(undefined, 'frame', 'variable') :
+        'next'
+      )
+    `,
+  ),
+  null,
+);
+
+// read rigid //
+assertEqual(
+  allignExpression(
+    makeLookupNode(
+      makeRigidDynamicFrame(false, makeLiteralExpression("frame")),
+      false,
+      "variable",
+      READ,
+      makeLiteralExpression("next"),
+    ),
+    `
+      (
+        intrinsic('aran.has')(undefined, 'frame', 'variable') ?
+        (
+          intrinsic('aran.binary')(
+            undefined,
+            '===',
+            intrinsic('aran.get')(undefined, 'frame', 'variable'),
+            intrinsic('aran.deadzone'),
+          ) ?
+          intrinsic('aran.throw')(
+            undefined,
+            new (intrinsic('ReferenceError'))(
+              'Cannot access \\'variable\\' before initialization',
+            ),
+          ) :
+          intrinsic('aran.get')(undefined, 'frame', 'variable')
+        ) :
+        'next'
+      )
+    `,
+  ),
+  null,
+);
+
+// read unscopable //
+assertEqual(
+  allignExpression(
+    makeLookupNode(
+      makeLooseDynamicFrame(true, makeLiteralExpression("frame")),
+      false,
+      "variable",
+      READ,
+      makeLiteralExpression("next"),
+    ),
+    `
+      (
+        (
+          intrinsic('aran.get')(
+            undefined,
+            'frame',
+            intrinsic('Symbol.unscopables'),
+          ) ?
+          (
+            intrinsic('aran.get')(
+              undefined,
+              intrinsic('aran.get')(
+                undefined,
+                'frame',
+                intrinsic('Symbol.unscopables'),
+              ),
+              'variable',
+            ) ?
+            false :
+            intrinsic('aran.has')(undefined, 'frame', 'variable')
+          ) :
+          intrinsic('aran.has')(undefined, 'frame', 'variable')
+        ) ?
+        intrinsic('aran.get')(undefined, 'frame', 'variable') :
+        'next'
+      )
+    `,
+  ),
+  null,
+);
+
+// delete //
+assertEqual(
+  allignExpression(
+    makeLookupNode(
+      makeLooseDynamicFrame(false, makeLiteralExpression("frame")),
+      false,
+      "variable",
+      DELETE,
+      makeLiteralExpression("next"),
+    ),
+    `
+      (
+        intrinsic('aran.has')(undefined, 'frame', 'variable') ?
+        intrinsic('aran.deleteSloppy')(undefined, 'frame', 'variable') :
+        'next'
+      )
+    `,
+  ),
+  null,
+);
+
+// write //
+// delete //
+assertEqual(
+  allignEffect(
+    makeLookupNode(
+      makeLooseDynamicFrame(false, makeLiteralExpression("frame")),
+      false,
+      "variable",
+      makeLiteralExpression("right"),
+      makeExpressionEffect(makeLiteralExpression("next")),
+    ),
+    `
+      (
+        intrinsic('aran.has')(undefined, 'frame', 'variable') ?
+        effect(
+          intrinsic('aran.setSloppy')(
+            undefined,
+            'frame',
+            'variable',
+            'right',
+          ),
+        ) :
+        effect('next')
+      )
+    `,
+  ),
+  null,
+);
