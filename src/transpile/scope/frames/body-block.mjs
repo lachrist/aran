8e438a2aaa40ebaef4d856601289fde59521d2b2
Laurent Christophe <lachrist@vub.ac.be>
2022-06-02 18:29:56 +0200

    refactor: first attempt at body-block

--- src/transpile/scope/frames/body-block.mjs
+++ src/transpile/scope/frames/body-block.mjs
@@ -1,81 +1,184 @@
-
-import {includes} from "array-lite";
-
-import {SyntaxAranError} from "../../../util.mjs";
+import {filter, map, concat, reduce, includes} from "array-lite";
 
 import {
-  BASE_KIND,
-  LOOSE_KIND,
-} from "../kind.mjs";
+  hasOwnProperty,
+  assert,
+  partialx_,
+  partial__x,
+} from "../../../util/index.mjs";
 
 import {
-  isReadRight,
-  isTypeofRight,
-  isDeleteRight,
-  getRightExpression,
-} from "../right.mjs";
+  makeConditionalExpression,
+  makeConditionalEffect,
+  makeEffectStatement,
+  makeLiteralExpression,
+  makeExpressionEffect,
+  makeReadExpression,
+  makeWriteEffect,
+} from "../../../ast/index.mjs";
+
+import {makeUnaryExpression} from "../../../intrinsic.mjs";
+
+import {makeVariable, makeShadowVariable} from "../variable.mjs";
+
+import {isRead, isTypeof, isDiscard, isWrite, accessWrite} from "../right.mjs";
+
+import {
+  makeThrowDeadzoneExpression,
+  makeExportStatement,
+  makeThrowConstantExpression,
+  makeExportSequenceEffect,
+} from "./helper.mjs";
 
 const {
-  Reflect: {ownKeys},
+  Reflect: {ownKeys, defineProperty},
 } = globalThis;
 
-const kinds = ["var", "function"];
+const kinds = ["let", "const", "class"];
 
-export const create = () => ({});
+const makeInitializedLookupNode = (binding, layer, variable, right) => {
+  if (isRead(right)) {
+    return makeReadExpression(makeVariable(layer, variable));
+  } else if (isTypeof(right)) {
+    return makeUnaryExpression(
+      "typeof",
+      makeReadExpression(makeVariable(layer, variable)),
+    );
+  } else if (isDiscard(right)) {
+    return makeLiteralExpression(false);
+  } else {
+    return binding.writable
+      ? reduce(
+          binding.exports,
+          partial__x(
+            makeExportSequenceEffect,
+            makeReadExpression(makeVariable(layer, variable)),
+          ),
+          makeWriteEffect(makeVariable(layer, variable), accessWrite(right)),
+        )
+      : makeExpressionEffect(makeThrowConstantExpression(variable));
+  }
+};
 
-export const harvestHeader = ownKeys;
+const hasDeadzone = (bindings, variable) => bindings[variable].deadzone;
 
-export const harvestPrelude = constant_([]);
+const makeShadowInitializeStatement = (layer, variable) =>
+  makeEffectStatement(
+    makeWriteEffect(
+      makeShadowVariable(layer, variable),
+      makeLiteralExpression(false),
+    ),
+  );
 
-export const makeDeclareStatementArray = (frame, kind, variable, import_, exports_) => {
+export const create = (layer) => ({
+  layer,
+  bindings: {},
+});
+
+export const harvest = ({layer, bindings}) => {
+  const variables = ownKeys(bindings);
+  const deadzone_variables = filter(
+    variables,
+    partialx_(hasDeadzone, bindings),
+  );
+  return {
+    header: concat(
+      map(variables, partialx_(makeVariable, layer)),
+      map(deadzone_variables, partialx_(makeShadowVariable, layer)),
+    ),
+    prelude: map(deadzone_variables, partialx_(layer, makeShadowInitializeStatement)),
+  };
+};
+
+export const makeDeclareStatements = (
+  _strict,
+  {bindings},
+  kind,
+  variable,
+  iimport,
+  eexports,
+) => {
   if (includes(kinds, kind)) {
-    assert(import_ === null, "unexpected imported variable");
-    assert(exports_.length === 0, "unexpected exported variable");
-
-  } else if (includes(frame, variable)) {
-    throw new AranSyntaxError("Duplicate variable declaration");
-  } else {
-    return null;
-  }
-};
-
-  assert(isBaseVariable(variable), "expected base variable");
-
-    if (!hasOwnProperty(frame, variable)) {
-
-    };
+    assert(iimport === null, "unexpected imported variable");
+    assert(!hasOwnProperty(bindings, variable), "duplicate variable");
+    defineProperty(bindings, variable, {
+      initialized: true,
+      deadzone: false,
+      writable: kind !== "const",
+      exports: eexports,
+    });
     return [];
-  } else if (hasOwnProperty(frame, variable)) {
-    assert(isBaseVariable(variable), "expected base variable");
-    throw new SyntaxAranError(`Duplicate variable declaration: ${variable}`);
   } else {
     return null;
   }
 };
 
-export const makeInitializeEffect = (frame, kind, variable, expression) => {
-  if (scopesKind(kind)) {
-    return makeWriteEffect(variable, expression);
+export const makeInitializeStatements = (_strict, {layer, bindings}, kind, variable, expression) => {
+  if (includes(kinds, kind)) {
+    assert(
+      hasOwnProperty(bindings, variable),
+      "missing variable for initialization",
+    );
+    const binding = bindings[variable];
+    assert(!binding.initialized, "duplicate initialization");
+    binding.initialized = true;
+    return concat(
+      [
+        makeEffectStatement(
+          makeWriteEffect(makeVariable(layer, variable), expression),
+        ),
+      ],
+      binding.deadzone
+        ? [makeEffectStatement(makeShadowVariable(layer, variable), true)]
+        : [],
+      map(
+        binding.exports,
+        partialx_(
+          makeExportStatement,
+          makeReadExpression(makeVariable(layer, variable)),
+        ),
+      ),
+    );
   } else {
-
+    return null;
   }
-  assert(includes(frame, variable), "missing variable for initialization");
-
 };
 
-export const makeLookupNode = (frame, _escaped, variable, right) => {
-  if (includes(frame, variable)) {
-    if (isReadRight(right)) {
-      return makeReadExpression(variable);
-    } else if (isTypeofRight(right)) {
-      return makeUnaryExpression(
-        "typeof",
-        makeReadExpression(variable),
-      );
-    } else if (isDeleteRight(right)) {
-      return makeLiteralExpression(true);
+export const makeLookupNode = (
+  next,
+  escaped,
+  variable,
+  {layer, bindings},
+  right,
+) => {
+  if (hasOwnProperty(bindings, variable)) {
+    const binding = bindings[variable];
+    if (binding.initialized) {
+      return makeInitializedLookupNode(binding, layer, variable, right);
     } else {
-      return makeWriteEffect(variable, getRightExpression(right));
+      if (isWrite(right)) {
+        if (escaped) {
+          binding.deadzone = true;
+          return makeConditionalEffect(
+            makeReadExpression(makeShadowVariable(layer, variable)),
+            makeInitializedLookupNode(binding, layer, variable, right),
+            makeExpressionEffect(makeThrowDeadzoneExpression(variable)),
+          );
+        } else {
+          return makeExpressionEffect(makeThrowDeadzoneExpression(variable));
+        }
+      } else {
+        if (escaped) {
+          binding.deadzone = true;
+          return makeConditionalExpression(
+            makeReadExpression(makeShadowVariable(layer, variable)),
+            makeInitializedLookupNode(binding, layer, variable, right),
+            makeThrowDeadzoneExpression(variable),
+          );
+        } else {
+          return makeThrowDeadzoneExpression(variable);
+        }
+      }
     }
   } else {
     return next();
---
+++ src/transpile/scope/frames/body-block.test.mjs
@@ -0,0 +1,73 @@
+import {assertSuccess} from "../../../__fixture__.mjs";
+
+import {makeLiteralExpression} from "../../../ast/index.mjs";
+
+import {testBlock} from "./__fixture__.mjs";
+
+import * as Frame from "./body-block.mjs";
+
+assertSuccess(
+  testBlock(Frame, {
+    head: "let VARIABLE;",
+    scenarios: [
+      {
+        type: "declare",
+        kind: "kind",
+      },
+      {
+        type: "declare",
+        kind: "var",
+        variable: "variable",
+        import: null,
+        exports: ["specifier"],
+        code: `
+          VARIABLE = undefined;
+          exportStatic('specifier', undefined);
+        `,
+      },
+      {
+        type: "initialize",
+        kind: "kind",
+      },
+      {
+        type: "initialize",
+        kind: "var",
+        variable: "variable",
+        right: makeLiteralExpression("right"),
+        code: `
+          VARIABLE = 'right';
+          exportStatic('specifier', VARIABLE);
+        `,
+      },
+      {
+        type: "read",
+        next: () => makeLiteralExpression("next"),
+        code: "'next'",
+      },
+      {
+        type: "read",
+        variable: "variable",
+        code: "VARIABLE",
+      },
+      {
+        type: "typeof",
+        variable: "variable",
+        code: "intrinsic.aran.unary('typeof', VARIABLE)",
+      },
+      {
+        type: "discard",
+        variable: "variable",
+        code: "false",
+      },
+      {
+        type: "write",
+        variable: "variable",
+        right: makeLiteralExpression("right"),
+        code: `(
+          VARIABLE = 'right',
+          exportStatic('specifier', VARIABLE)
+        )`,
+      },
+    ],
+  }),
+);
