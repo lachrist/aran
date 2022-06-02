6f25573a544f741431ed41da598c5df3e2ebdf53
Laurent Christophe <lachrist@vub.ac.be>
2022-06-02 12:46:31 +0200

    refactor: add more helpers for frames

--- src/transpile/scope/frames/helper.mjs
+++ src/transpile/scope/frames/helper.mjs
@@ -1,8 +1,20 @@
+import {partial_x} from "../../../util/index.mjs";
+
+import {
+  makeEffectStatement,
+  makeExportEffect,
+  makeLiteralExpression,
+  makeSequenceEffect,
+} from "../../../ast/index.mjs";
+
 import {
   makeGetExpression,
   makeUnaryExpression,
   makeDeleteExpression,
   makeSetExpression,
+  makeThrowReferenceErrorExpression,
+  makeThrowTypeErrorExpression,
+  makeThrowSyntaxErrorExpression,
 } from "../../../intrinsic.mjs";
 
 import {isRead, isTypeof, isDiscard, accessWrite} from "../right.mjs";
@@ -18,3 +30,32 @@ export const makeDynamicLookupExpression = (strict, object, key, right) => {
     return makeSetExpression(strict, object, key, accessWrite(right));
   }
 };
+
+export const makeThrowDuplicateExpression = (variable) =>
+  makeThrowSyntaxErrorExpression(
+    `Variable '${variable}' has already been declared`,
+  );
+
+export const makeThrowMissingExpression = (variable) =>
+  makeThrowReferenceErrorExpression(`Variable '${variable}' is not defined`);
+
+export const makeThrowDeadzoneExpression = (variable) =>
+  makeThrowReferenceErrorExpression(
+    `Cannot access variable '${variable}' before initialization`,
+  );
+
+export const makeThrowConstantExpression = (variable) =>
+  makeThrowTypeErrorExpression(
+    `Cannot assign variable '${variable}' because it is a constant`,
+  );
+
+export const makeExportStatement = (specifier, expression) =>
+  makeEffectStatement(makeExportEffect(specifier, expression));
+
+export const makeExportUndefinedStatement = partial_x(
+  makeExportStatement,
+  makeLiteralExpression({undefined: null}),
+);
+
+export const makeExportSequenceEffect = (effect, specifier, expression) =>
+  makeSequenceEffect(effect, makeExportEffect(specifier, expression));
--- src/transpile/scope/frames/helper.test.mjs
+++ src/transpile/scope/frames/helper.test.mjs
@@ -1,12 +1,28 @@
 import {assertSuccess} from "../../../__fixture__.mjs";
 
-import {makeLiteralExpression} from "../../../ast/index.mjs";
+import {
+  makeExpressionEffect,
+  makeLiteralExpression,
+} from "../../../ast/index.mjs";
 
-import {allignExpression} from "../../../allign/index.mjs";
+import {
+  allignEffect,
+  allignStatement,
+  allignExpression,
+} from "../../../allign/index.mjs";
 
 import {makeRead, makeTypeof, makeDiscard, makeWrite} from "../right.mjs";
 
-import {makeDynamicLookupExpression} from "./helper.mjs";
+import {
+  makeDynamicLookupExpression,
+  makeThrowDuplicateExpression,
+  makeThrowMissingExpression,
+  makeThrowDeadzoneExpression,
+  makeThrowConstantExpression,
+  makeExportStatement,
+  makeExportUndefinedStatement,
+  makeExportSequenceEffect,
+} from "./helper.mjs";
 
 assertSuccess(
   allignExpression(
@@ -53,5 +69,58 @@ assertSuccess(
       makeWrite(makeLiteralExpression("right")),
     ),
     "intrinsic.aran.setSloppy('object', 'key', 'right')",
+  ),
+);
+
+assertSuccess(
+  allignExpression(
+    makeThrowDuplicateExpression("variable"),
+    `intrinsic.aran.throw(new intrinsic.SyntaxError("Variable 'variable' has already been declared"))`,
+  ),
+);
+
+assertSuccess(
+  allignExpression(
+    makeThrowMissingExpression("variable"),
+    `intrinsic.aran.throw(new intrinsic.ReferenceError("Variable 'variable' is not defined"))`,
+  ),
+);
+
+assertSuccess(
+  allignExpression(
+    makeThrowDeadzoneExpression("variable"),
+    `intrinsic.aran.throw(new intrinsic.ReferenceError("Cannot access variable 'variable' before initialization"))`,
+  ),
+);
+
+assertSuccess(
+  allignExpression(
+    makeThrowConstantExpression("variable"),
+    `intrinsic.aran.throw(new intrinsic.TypeError("Cannot assign variable 'variable' because it is a constant"))`,
+  ),
+);
+
+assertSuccess(
+  allignStatement(
+    makeExportStatement("specifier", makeLiteralExpression(123)),
+    `exportStatic('specifier', 123);`,
+  ),
+);
+
+assertSuccess(
+  allignStatement(
+    makeExportUndefinedStatement("specifier"),
+    `exportStatic('specifier', undefined);`,
+  ),
+);
+
+assertSuccess(
+  allignEffect(
+    makeExportSequenceEffect(
+      makeExpressionEffect(makeLiteralExpression(123)),
+      "specifier",
+      makeLiteralExpression(456),
+    ),
+    `(effect(123), exportStatic('specifier', 456))`,
   ),
 );
