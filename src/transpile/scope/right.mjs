941b4e44d2f430807493b8cc93a9b442179eb910
Laurent Christophe <lachrist@vub.ac.be>
2022-06-08 16:57:32 +0200

    performance: remove counter from write right and use direct property

--- src/transpile/scope/right.mjs
+++ src/transpile/scope/right.mjs
@@ -1,12 +1,4 @@
-import {
-  constant,
-  createCounter,
-  incrementCounter,
-  gaugeCounter,
-  assert,
-  equals,
-  partialx_,
-} from "../../util/index.mjs";
+import {constant, assert, equals, partialx_} from "../../util/index.mjs";
 
 const {Symbol} = globalThis;
 
@@ -32,19 +24,17 @@ export const isWrite = (right) =>
   right !== READ && right !== TYPEOF && right !== DISCARD;
 
 export const makeWrite = (expression) => ({
-  counter: createCounter(0),
+  access: 0,
   expression,
 });
 
 export const accessWrite = (right) => {
   assert(isWrite(right), "expected write right");
-  const {counter, expression} = right;
-  incrementCounter(counter);
-  return expression;
+  right.access += 1;
+  return right.expression;
 };
 
 export const accountWrite = (right) => {
   assert(isWrite(right), "expected write right");
-  const {counter} = right;
-  return gaugeCounter(counter);
+  return right.access;
 };
