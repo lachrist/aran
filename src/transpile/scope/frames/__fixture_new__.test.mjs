6ffc14f6b3a8feeeab17fcc241aca78b28310866
Laurent Christophe <lachrist@vub.ac.be>
2023-02-18 17:50:09 +0100

    refactor: alternative fixture for testing frame instances

---
+++ src/transpile/scope/frames/__fixture_new__.test.mjs
@@ -0,0 +1,54 @@
+import { assertEqual, assertSuccess } from "../../../__fixture__.mjs";
+
+import {
+  returnx,
+  constant_,
+  constant___,
+  constant_____,
+  constant_______,
+} from "../../../util/index.mjs";
+
+import {
+  makeEffectStatement,
+  makeExpressionEffect,
+  makeLiteralExpression,
+} from "../../../ast/index.mjs";
+
+import { allignBlock } from "../../../allign/index.mjs";
+
+import { assertFrameLibrary, makeFrameBlock } from "./__fixture_new__.mjs";
+
+const { undefined } = globalThis;
+
+const Frame = {
+  createFrame: returnx,
+  harvestFrameHeader: constant_(["variable"]),
+  harvestFramePrelude: constant_([
+    makeEffectStatement(makeExpressionEffect(makeLiteralExpression("prelude"))),
+  ]),
+  declareFrame: constant_____(undefined),
+  makeFrameInitializeStatementArray: constant_____([]),
+  lookupFrameAll: constant___(undefined),
+  makeFrameReadExpression: constant_______(makeLiteralExpression("read")),
+  makeFrameTypeofExpression: constant_______(makeLiteralExpression("typeof")),
+  makeFrameDiscardExpression: constant_______(makeLiteralExpression("discard")),
+  makeFrameWriteEffect: constant_______(
+    makeExpressionEffect(makeLiteralExpression("read")),
+  ),
+};
+
+assertFrameLibrary(Frame);
+
+assertSuccess(
+  allignBlock(
+    makeFrameBlock(Frame, "options", (frame) => {
+      assertEqual(frame, "options");
+      return [
+        makeEffectStatement(
+          makeExpressionEffect(makeLiteralExpression("body")),
+        ),
+      ];
+    }),
+    `{ let variable; void "prelude"; void "body"; }`,
+  ),
+);
