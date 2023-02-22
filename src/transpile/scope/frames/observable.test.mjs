c67855af330b89c5f2a7f58a4e78249464ea244f
Laurent Christophe <lachrist@vub.ac.be>
2023-02-22 09:54:10 +0100

    refactor: remove redundant tests

--- src/transpile/scope/frames/observable.test.mjs
+++ src/transpile/scope/frames/observable.test.mjs
@@ -4,31 +4,15 @@ import {
   makeLiteralExpression,
   makeExpressionEffect,
 } from "../../../ast/index.mjs";
-import { allignEffect, allignExpression } from "../../../allign/index.mjs";
+import { allignEffect } from "../../../allign/index.mjs";
 import { assertFrameLibrary } from "./__fixture_new__.mjs";
 import * as Frame from "./observable.mjs";
 
 const STRICT = true;
 const ESCAPED = true;
-const { createFrame, makeFrameReadExpression, makeFrameWriteEffect } = Frame;
+const { createFrame, makeFrameWriteEffect } = Frame;
 
 assertFrameLibrary(Frame);
-
-assertSuccess(
-  allignExpression(
-    makeFrameReadExpression(
-      (_strict, _scope, _variable, _escape, _options) =>
-        makeLiteralExpression("next"),
-      STRICT,
-      createFrame({}),
-      "scope",
-      "variable",
-      ESCAPED,
-      "options",
-    ),
-    `"next"`,
-  ),
-);
 
 {
   const counter = createCounter(0);
--- src/transpile/scope/frames/trail.test.mjs
+++ src/transpile/scope/frames/trail.test.mjs
@@ -1,17 +1,10 @@
-import { assertDeepEqual, assertSuccess } from "../../../__fixture__.mjs";
+import { assertDeepEqual } from "../../../__fixture__.mjs";
 import { makeLiteralExpression } from "../../../ast/index.mjs";
-import { allignExpression } from "../../../allign/index.mjs";
 import { assertFrameLibrary } from "./__fixture_new__.mjs";
 import * as Frame from "./trail.mjs";
 
 const STRICT = true;
-const ESCAPED = true;
-const {
-  createFrame,
-  declareFrame,
-  makeFrameReadExpression,
-  makeFrameInitializeStatementArray,
-} = Frame;
+const { createFrame, declareFrame, makeFrameInitializeStatementArray } = Frame;
 
 assertFrameLibrary(Frame);
 
@@ -39,19 +32,4 @@ assertDeepEqual(
     makeLiteralExpression("right"),
   ),
   { program: true, distant: false },
-);
-
-assertSuccess(
-  allignExpression(
-    makeFrameReadExpression(
-      (_strict, _scope, _escaped, _variable, _options) =>
-        makeLiteralExpression("next"),
-      frame,
-      "scope",
-      ESCAPED,
-      "variable",
-      {},
-    ),
-    `"next";`,
-  ),
 );
