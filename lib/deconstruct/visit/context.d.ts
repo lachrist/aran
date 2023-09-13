22cee620766849a7aca87f7a02a5db038ed29398
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-09-13 12:49:01 +0200

    refactor: progress on deconstruct

--- lib/deconstruct/visit/context.d.ts
+++ lib/deconstruct/visit/context.d.ts
@@ -1,5 +1,6 @@
 export type Context<S> = {
   strict: boolean;
+  super: Variable | null;
   scope: import("../scope/index.mjs").Scope<S>;
   serialize: (node: estree.Node) => S;
   mangle: (node: estree.Node, site: string, purpose: string) => Variable;
