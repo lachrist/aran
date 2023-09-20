af7f7464c20728085a43880548bc353c7429e5a9
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-09-20 16:07:59 +0200

    refactor: more robust binding for meta variables

--- type/unbuild.d.ts
+++ type/unbuild.d.ts
@@ -2,12 +2,15 @@ declare namespace unbuild {
   type Variable = Brand<string, "unbuild.Variable">;
   type Label = Brand<string, "unbuild.Label">;
   type Hash = Brand<string, "unbuild.Hash">;
-  type Atom<T> = {
+  type Atom<S> = {
     Label: Label;
     Source: estree.Source;
     Specifier: estree.Specifier;
     Variable: Variable;
     EnclaveVariable: estree.Variable;
-    Tag: T;
+    Tag: {
+      serial: S;
+      initialization: Variable | null;
+    };
   };
 }
