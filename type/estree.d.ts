5cc4acfb321141a76c5055df332235577cfa749e
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-09-21 16:29:34 +0200

    feat: unbuild spread and callee

--- type/estree.d.ts
+++ type/estree.d.ts
@@ -11,6 +11,8 @@ declare namespace estree {
   type Specifier = Brand<string, "estree.Specifier">;
   type Source = Brand<string, "estree.Source">;
   type Super = import("../node_modules/@types/estree/index.d.ts").Super;
+  type SpreadElement =
+    import("../node_modules/@types/estree/index.d.ts").SpreadElement;
   type RegExpLiteral =
     import("../node_modules/@types/estree/index.d.ts").RegExpLiteral;
   type Identifier =
