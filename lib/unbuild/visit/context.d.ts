a5dceff6b987e58664e36dfd691c5c1368826401
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-09-28 11:19:42 +0200

    feat: completion

--- lib/unbuild/visit/context.d.ts
+++ lib/unbuild/visit/context.d.ts
@@ -1,12 +1,13 @@
-import { Scope } from "../scope/inner/index.mjs";
+import { Scope } from "../scope/index.mjs";
 import { Private } from "../private.mjs";
 
 export type Context<S> = {
+  completion: unbuild.Variable | null;
   escape: estree.Variable;
   strict: boolean;
   break: unbuild.Label | null;
   continue: unbuild.Label | null;
-  scope: Scope<S>;
+  scope: Scope;
   serialize: (node: estree.Node) => S;
   digest: (node: estree.Node) => unbuild.Hash;
   private: Private;
