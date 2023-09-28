0c5298dda7f578183a530f50249eea60cd1f5482
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-09-28 16:23:42 +0200

    refactor: simplify scope structure

--- lib/unbuild/context.d.ts
+++ lib/unbuild/context.d.ts
@@ -1,32 +1,41 @@
 import { Scope } from "./scope/index.mjs";
 import { Private } from "./private.mjs";
 
+type Record = {
+  "this":
+    | ".illegal"
+    | ".undefined"
+    | ".global"
+    | aran.Parameter
+    | unbuild.Variable;
+  "import.meta": ".illegal" | aran.Parameter | unbuild.Variable;
+  "new.target": ".illegal" | aran.Parameter | unbuild.Variable;
+  "super.constructor":
+    | ".illegal"
+    | ".enclave"
+    | ".default"
+    | aran.Parameter
+    | unbuild.Variable;
+  "super.post": ".none" | aran.Parameter | unbuild.Variable;
+  "super.prototype":
+    | ".illegal"
+    | ".enclave"
+    | aran.Parameter
+    | unbuild.Variable;
+};
+
 export type Context<S> = {
   strict: boolean;
   scope: Scope;
   serialize: (node: estree.Node) => S;
   digest: (node: estree.Node) => unbuild.Hash;
   private: Private;
-  record: {
-    "this":
-      | ".illegal"
-      | ".undefined"
-      | ".global"
-      | aran.Parameter
-      | unbuild.Variable;
-    "import.meta": ".illegal" | aran.Parameter | unbuild.Variable;
-    "new.target": ".illegal" | aran.Parameter | unbuild.Variable;
-    "super.constructor":
-      | ".illegal"
-      | ".enclave"
-      | ".default"
-      | aran.Parameter
-      | unbuild.Variable;
-    "super.post": ".none" | aran.Parameter | unbuild.Variable;
-    "super.prototype":
-      | ".illegal"
-      | ".enclave"
-      | aran.Parameter
-      | unbuild.Variable;
-  };
+  record: Record;
+};
+
+export type PackContext = {
+  strict: boolean;
+  scope: Scope;
+  private: Private;
+  record: Record;
 };
