cbae348829c127e5620ce004d4e71661651114bd
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-10-19 21:14:16 +0200

    feat: signal block function declaration

--- type/unbuild.d.ts
+++ type/unbuild.d.ts
@@ -14,6 +14,10 @@ export type Log =
   | {
       name: "EnclaveLimitation";
       message: string;
+    }
+  | {
+      name: "BlockFunctionDeclaration";
+      message: string;
     };
 
 export type Atom = {
