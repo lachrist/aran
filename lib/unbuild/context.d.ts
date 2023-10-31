1dd128d3442e35bf47244c33dc278407c27eacfc
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-10-31 20:47:56 +0100

    refactor: more explicit parameter system

--- lib/unbuild/context.d.ts
+++ lib/unbuild/context.d.ts
@@ -2,31 +2,9 @@ import { Scope } from "./scope/index.mjs";
 import { Private } from "./private.mjs";
 import type { Root } from "../../type/options.d.ts";
 
-type Record = {
-  "import.meta": ".illegal" | aran.Parameter | unbuild.Variable;
-  "this":
-    | ".illegal"
-    | ".undefined"
-    | ".global"
-    | ".derived"
-    | aran.Parameter
-    | unbuild.Variable;
-  "new.target": ".illegal" | aran.Parameter | unbuild.Variable;
-  "super.constructor":
-    | ".illegal"
-    | ".enclave"
-    | ".default"
-    | aran.Parameter
-    | unbuild.Variable;
-  "super.self": ".illegal" | ".enclave" | aran.Parameter | unbuild.Variable;
-  "class.field": ".illegal" | ".none" | aran.Parameter | unbuild.Variable;
-  "function.arguments": ".illegal" | aran.Parameter | unbuild.Variable;
-};
-
 export type Context = {
   strict: boolean;
   root: Root;
   scope: Scope;
   private: Private;
-  record: Record;
 };
