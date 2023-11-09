f219d2c89968f7f4a2272e2603e9340358f1c162
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-09 11:19:25 +0100

    refactor: separate param from scope

--- lib/unbuild/context.d.ts
+++ lib/unbuild/context.d.ts
@@ -1,10 +1,12 @@
 import { Scope } from "./scope/index.mjs";
 import { Private } from "./private.mjs";
 import type { Root } from "../../type/options.d.ts";
+import { Param } from "./param.mjs";
 
 export type Context = {
   strict: boolean;
   root: Root;
   scope: Scope;
   private: Private;
+  params: Param[];
 };
