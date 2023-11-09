724fb63bd0fa182f3a6d77f5f8f078c8b2b39af2
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-09 11:57:49 +0100

    refactor: seperate simple params from param

--- lib/unbuild/context.d.ts
+++ lib/unbuild/context.d.ts
@@ -1,13 +1,14 @@
 import { Scope } from "./scope/index.mjs";
 import { Private } from "./private.mjs";
 import type { Root } from "../../type/options.d.ts";
-import { Param } from "./param.mjs";
+import type { Param } from "./param/param.d.ts";
+import type { Param as OldParam } from "./param.mjs";
 
 export type Context = {
-  "strict": boolean;
-  "root": Root;
-  "scope": Scope;
-  "private": Private;
-  "params": Param[];
-  "catch.error": boolean;
+  strict: boolean;
+  root: Root;
+  scope: Scope;
+  param: Param;
+  private: Private;
+  params: OldParam[];
 };
---
+++ lib/unbuild/param/param.d.ts
@@ -0,0 +1,7 @@
+export type Param = {
+  "catch.error": boolean;
+  "new.target": boolean;
+  "function.arguments": boolean;
+  "import.meta": boolean;
+  "import.dynamic": boolean;
+};
