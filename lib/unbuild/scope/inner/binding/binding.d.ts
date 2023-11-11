f6538288f89af8a07ededa5815d4dc7717bd7a31
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-11 13:07:22 +0100

    refactor: on demand binding computation

--- lib/unbuild/scope/inner/binding/binding.d.ts
+++ lib/unbuild/scope/inner/binding/binding.d.ts
@@ -2,18 +2,20 @@ type ImportBinding = {
   type: "import";
   source: estree.Source;
   specifier: estree.Specifier | null;
+  variable: estree.Variable;
 };
 
 type RegularBinding = {
   type: "regular";
   kind: "let" | "const" | "var";
-  internalized: boolean;
   exports: estree.Specifier[];
+  variable: estree.Variable;
 };
 
 type RootBinding = {
   type: "root";
   kind: "let" | "const" | "var" | "missing";
+  variable: estree.Variable;
 };
 
 type Binding = ImportBinding | RegularBinding | RootBinding;
