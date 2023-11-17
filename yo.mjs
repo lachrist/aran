7baec00beb51a98a9b83955916909e0aacee278b
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-17 12:57:46 +0100

    refactor: switch iterator - iterable
    I mixed the two :|

--- yo.mjs
+++ yo.mjs
@@ -1,45 +1,26 @@
-// This file was procedurally generated from the following sources:
-// - src/dstr-assignment/array-elem-iter-nrml-close-null.case
-// - src/dstr-assignment/error/assignment-expr.template
-/*---
-description: IteratorClose throws a TypeError when `return` returns a non-Object value (AssignmentExpression)
-esid: sec-variable-statement-runtime-semantics-evaluation
-features: [Symbol.iterator, destructuring-binding]
-flags: [generated]
-info: |
-    VariableDeclaration : BindingPattern Initializer
-
-    1. Let rhs be the result of evaluating Initializer.
-    2. Let rval be GetValue(rhs).
-    3. ReturnIfAbrupt(rval).
-    4. Return the result of performing BindingInitialization for
-       BindingPattern passing rval and undefined as arguments.
-
-    ArrayAssignmentPattern : [ AssignmentElementList ]
-
-    [...]
-    5. If iteratorRecord.[[done]] is false, return IteratorClose(iterator,
-       result).
-    6. Return result.
-
-    7.4.6 IteratorClose( iterator, completion )
-
-    [...]
-    6. Let innerResult be Call(return, iterator, « »).
-    7. If completion.[[type]] is throw, return Completion(completion).
-    8. If innerResult.[[type]] is throw, return Completion(innerResult).
-    9. If Type(innerResult.[[value]]) is not Object, throw a TypeError
-       exception.
-
----*/
-
-const iterator = {
-  [Symbol.iterator]: () => ({
-    next: () => {
-      console.log("next");
-      return 123;
-    },
-  }),
+var returnCount = 0;
+var unreachable = 0;
+var iterable = {};
+var iterator = {
+  return: function () {
+    returnCount += 1;
+    throw "BOUM";
+  },
+};
+var iter;
+iterable[Symbol.iterator] = function () {
+  return iterator;
 };
 
-const [x] = iterator;
+function* g() {
+  var result;
+  var vals = iterable;
+
+  result = [{}[yield]] = vals;
+
+  unreachable += 1;
+}
+
+iter = g();
+iter.next();
+iter.return();
