b4826e97951c6cc6c37ccb28f2502904c7b4fe85
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-17 09:18:23 +0100

    refactor: restore wrapStatement in pattern

--- yo.mjs
+++ yo.mjs
@@ -2,12 +2,14 @@ const g = {
   [Symbol.iterator]: () => ({
     next: (...args) => {
       console.log("next", args);
-      return { value: undefined, done: false };
+      return { value: 123, done: true };
     },
     return: (...args) => {
       console.log("return", args);
+      return {};
     },
   }),
 };
 
 var [x, y, z] = g;
+console.log({ x, y, z });
