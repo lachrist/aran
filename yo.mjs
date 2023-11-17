afa0df6ea63a722daec1b51a57139fb3fff82736
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-17 16:49:35 +0100

    fix: class accessor name

--- yo.mjs
+++ yo.mjs
@@ -2,3 +2,18 @@
 // runInThisContext(``);
 
 let {} = 123;
+
+var count = 0;
+class C {
+  static m() {
+    super[0] = count += 1;
+  }
+}
+
+Object.setPrototypeOf(C, null);
+
+assert.throws(TypeError, function () {
+  C.m();
+});
+
+assert.sameValue(count, 1);
