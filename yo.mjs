1a4bf2c1b5b2526eea719448d0cc46f8949ae0e4
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-21 19:30:45 +0100

    fix: delay key convertion for update member

--- yo.mjs
+++ yo.mjs
@@ -15,11 +15,13 @@
 
 // // // console.log(CC.foo.name);
 
-function f() {
-  console.log("foo", new.target);
-  class C extends (console.log("qux", new.target), null) {
-    static foo = console.log("bar", new.target);
-  }
-}
-
-new f();
+console.log(
+  {
+    [{
+      toString: () => {
+        console.log("convert");
+        return "foo";
+      },
+    }]: () => {},
+  }.foo.name,
+);
