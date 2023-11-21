24dee7f65ce33a6cc511ecf23858ad23c1880535
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-21 19:06:40 +0100

    fix: class scope for self

--- yo.mjs
+++ yo.mjs
@@ -1,5 +1,25 @@
-import { runInThisContext } from "vm";
-runInThisContext(`
-  function f([]) {}
-  console.log(Object.getOwnPropertyDescriptors(f));
-`);
+// let yo = null;
+
+// const CC = class C extends ((yo = () => C), null) {};
+
+// const CC = (() => {
+//   let C;
+
+// } ());
+
+// console.log(yo());
+
+// // const c = new CC();
+
+// // console.log(Object.getOwnPropertyDescriptors(c));
+
+// // // console.log(CC.foo.name);
+
+function f() {
+  console.log("foo", new.target);
+  class C extends (console.log("qux", new.target), null) {
+    static foo = console.log("bar", new.target);
+  }
+}
+
+new f();
