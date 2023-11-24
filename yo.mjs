23067736135547a1745938722f08df8911115856
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-24 10:04:33 +0100

    fix: test for await|yield in manufactured closures

--- yo.mjs
+++ yo.mjs
@@ -1,11 +1,13 @@
-import { x as xx } from "./yo.mjs";
-
-try {
-  console.log(xx);
-} catch (e) {
-  console.log(e);
+async function* f() {
+  const x = class C extends (await (yield* (async function* () {
+    await 123;
+    return yield 123, Object;
+  })())) {
+    constructor() {}
+  };
+  yield x;
 }
 
-export const x = 123;
-
-console.log(xx);
+for await (const x of f()) {
+  console.log(x);
+}
