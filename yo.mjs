a81bee7e6a709bdfe669a57b35b4cb7601650887
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-11-29 10:14:57 +0100

    fix: dedicated variable per for-loop iteration

--- yo.mjs
+++ yo.mjs
@@ -1,17 +1,29 @@
-class D {
-  constructor() {
-    return { __proto__: null };
-  }
-  foo() {
-    console.log("foo");
-  }
+const fs = [];
+for (let x = 0; x < 10; x += 1) {
+  fs.push(() => ({ x }));
+}
+for (const f of fs) {
+  console.log(f());
 }
 
-class C extends D {
-  constructor() {
-    super();
-    super.foo();
+{
+  let x, y, _x, _y, _test;
+  x = 123;
+  y = 456;
+  _x = x;
+  _y = y;
+  _first = true;
+  _test = true;
+  while (_first || _test) {
+    let x, y;
+    x = _x;
+    y = _y;
+    _first ? (first = false) : (x += 1);
+    _test = x < 10;
+    if (_test) {
+      fs.push(() => ({ x, y }));
+    }
+    _x = x;
+    _y = y;
   }
 }
-
-new C();
