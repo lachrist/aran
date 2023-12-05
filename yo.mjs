df1188c75b1c7c927d3e4c82db5eeb1beaa80a0b
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-12-05 16:51:38 +0100

    fix: no longer call return at each iteration

--- yo.mjs
+++ yo.mjs
@@ -1,49 +1,54 @@
-// const iterator = {
-//   [Symbol.iterator]: () => {
-//     return {
-//       next: () => {
-//         console.log("next");
-//         // throw new Error("NEXT");
-//         return { done: false, value: 123 };
-//       },
-//       throw: () => {
-//         console.log("throw");
-//       },
-//       return: () => {
-//         console.log("return");
-//         throw new Error("RETURN");
-//       },
-//     };
-//   },
-// };
+const iterator = {
+  [Symbol.iterator]: () => {
+    return {
+      next: () => {
+        console.log("next");
+        throw new Error("NEXT");
+        return { done: false, value: 123 };
+      },
+      throw: () => {
+        console.log("throw");
+      },
+      return: () => {
+        console.log("return");
+        throw new Error("RETURN");
+      },
+    };
+  },
+};
 
-// for (const [x] of iterator) {
-//   console.log({ x });
-//   throw new Error("BODY");
-// }
-
-for (const x of [1, 2, 3]) {
+for (const [x] of iterator) {
   console.log({ x });
+  throw new Error("BODY");
 }
+
+// for (const x of [1, 2, 3]) {
+//   console.log({ x });
+// }
 
 const iterator = [1, 2, 3];
 const iterable = iterator[Symbol.iterable]();
 let step = iterable.next();
-let caught = false;
-while (!step.done) {
-  try {
-    const x = step.value;
-    console.log({ x });
-  } catch (error) {
-    caught = true;
+try {
+  while (!step.done) {
     try {
-      iterable.return();
-    } catch {}
-    throw error;
-  } finally {
-    if (!caught && !next.done) {
-      iterable.return();
+      const x = step.value;
+      console.log({ x });
+    } catch (error) {
+      try {
+        iterable.return();
+      } catch {}
+      throw error;
     }
+    step = iterable.next();
   }
-  step = iterable.next();
+} catch (error) {
+  step = {
+    done: true,
+    value: undefined,
+  };
+} finally {
+  if (!step.done) {
+    iterable.return();
+  }
 }
