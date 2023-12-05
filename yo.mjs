ed9a9f30443498930d53a17ee2ef8b1ffc8c99fa
Laurent Christophe <laurent.christophe.terken@gmail.com>
2023-12-05 18:00:56 +0100

    fix: only get next once in for-of

--- yo.mjs
+++ yo.mjs
@@ -1,54 +1,16 @@
-const iterator = {
-  [Symbol.iterator]: () => {
-    return {
-      next: () => {
-        console.log("next");
-        throw new Error("NEXT");
-        return { done: false, value: 123 };
-      },
-      throw: () => {
-        console.log("throw");
-      },
-      return: () => {
-        console.log("return");
-        throw new Error("RETURN");
-      },
-    };
-  },
+let index = 0;
+const iterable = {
+  [Symbol.iterator]: () => ({
+    get next() {
+      console.log("get next");
+      return () => ({
+        done: index > 10,
+        value: (index += 1),
+      });
+    },
+  }),
 };
 
-for (const [x] of iterator) {
-  console.log({ x });
-  throw new Error("BODY");
-}
-
-// for (const x of [1, 2, 3]) {
-//   console.log({ x });
-// }
-
-const iterator = [1, 2, 3];
-const iterable = iterator[Symbol.iterable]();
-let step = iterable.next();
-try {
-  while (!step.done) {
-    try {
-      const x = step.value;
-      console.log({ x });
-    } catch (error) {
-      try {
-        iterable.return();
-      } catch {}
-      throw error;
-    }
-    step = iterable.next();
-  }
-} catch (error) {
-  step = {
-    done: true,
-    value: undefined,
-  };
-} finally {
-  if (!step.done) {
-    iterable.return();
-  }
+for (const x of iterable) {
+  console.log(x);
 }
