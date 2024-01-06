7a78077dc7f998e2a7d837b9e630abc542f91909
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-01-06 14:02:36 +0100

    refactor: explicit rebuild design

---
+++ lib/rebuild/design.d.ts
@@ -0,0 +1,105 @@
+import { BinaryOperator, UnaryOperator } from "estree";
+
+export type MemberDesign = aran.Expression<rebuild.Atom> & {
+  type: "ApplyExpression";
+  callee: aran.Expression<rebuild.Atom> & {
+    type: "IntrinsicExpression";
+    intrinsic: "aran.get";
+  };
+  this: aran.Expression<rebuild.Atom> & {
+    type: "PrimitiveExpression";
+  };
+  arguments: [aran.Expression<rebuild.Atom>, aran.Expression<rebuild.Atom>];
+};
+
+export type ArrayDesign = aran.Expression<rebuild.Atom> & {
+  type: "ApplyExpression";
+  callee: aran.Expression<rebuild.Atom> & {
+    type: "IntrinsicExpression";
+    intrinsic: "Array.of";
+  };
+  this: aran.Expression<rebuild.Atom> & {
+    type: "PrimitiveExpression";
+  };
+};
+
+export type ListDesign<X> = aran.Expression<rebuild.Atom> & {
+  type: "ApplyExpression";
+  callee: aran.Expression<rebuild.Atom> & {
+    type: "IntrinsicExpression";
+    intrinsic: "Array.of";
+  };
+  this: aran.Expression<rebuild.Atom> & {
+    type: "PrimitiveExpression";
+  };
+  arguments: X[];
+};
+
+export type PairDesign<X, Y> = aran.Expression<rebuild.Atom> & {
+  type: "ApplyExpression";
+  callee: aran.Expression<rebuild.Atom> & {
+    type: "IntrinsicExpression";
+    intrinsic: "Array.of";
+  };
+  this: aran.Expression<rebuild.Atom> & {
+    type: "PrimitiveExpression";
+  };
+  arguments: [X, Y];
+};
+
+export type ObjectDesign = aran.Expression<rebuild.Atom> & {
+  callee: aran.Expression<rebuild.Atom> & {
+    type: "IntrinsicExpression";
+    intrinsic: "Object.fromEntries";
+  };
+  this: aran.Expression<rebuild.Atom> & {
+    type: "PrimitiveExpression";
+  };
+  arguments: [
+    ListDesign<
+      PairDesign<
+        aran.Expression<rebuild.Atom> & {
+          type: "PrimitiveExpression";
+        },
+        aran.Expression<rebuild.Atom> & {
+          type: "PrimitiveExpression";
+        }
+      >
+    >,
+  ];
+};
+
+export type UnaryDesign = aran.Expression<rebuild.Atom> & {
+  callee: aran.Expression<rebuild.Atom> & {
+    type: "IntrinsicExpression";
+    intrinsic: "aran.unary";
+  };
+  this: aran.Expression<rebuild.Atom> & {
+    type: "PrimitiveExpression";
+  };
+  arguments: [
+    aran.Expression<rebuild.Atom> & {
+      type: "PrimitiveExpression";
+      primitive: UnaryOperator;
+    },
+    aran.Expression<rebuild.Atom>,
+  ];
+};
+
+export type BinaryDesign = aran.Expression<rebuild.Atom> & {
+  callee: aran.Expression<rebuild.Atom> & {
+    type: "IntrinsicExpression";
+    intrinsic: "aran.unary";
+  };
+  this: aran.Expression<rebuild.Atom> & {
+    type: "PrimitiveExpression";
+  };
+  arguments: [
+    aran.Expression<rebuild.Atom> & {
+      type: "PrimitiveExpression";
+      primitive: BinaryOperator;
+    },
+    aran.Expression<rebuild.Atom>,
+    aran.Expression<rebuild.Atom>,
+  ];
+};
