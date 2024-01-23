b588592c491f6655c9a1d2e63da679393c0d3a34
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-01-23 17:41:09 +0100

    test: tweak test parsing to favor acorn

--- test/262/parse.mjs
+++ test/262/parse.mjs
@@ -2,6 +2,7 @@ import { parse as parseAcorn } from "acorn";
 import { parse as parseBabel } from "@babel/parser";
 
 const {
+  undefined,
   Object: { hasOwn, values: listValue },
   Array: { isArray },
   SyntaxError,
@@ -59,11 +60,63 @@ export const parseGlobal = (code, base, kind) => {
 };
 
 /**
+ * @type {<B>(
+ *   code: string,
+ *   base: B,
+ *   context: import("../../lib/context").InternalLocalContext,
+ * ) => import("./outcome").Outcome<
+ *   import("../../lib/program").InternalLocalEvalProgram<B>,
+ *   string
+ * >}
+ */
+export const parseAcornLocal = (code, base, context) => {
+  try {
+    return {
+      type: "success",
+      data: {
+        kind: "eval",
+        root: /** @type {any} */ (
+          parseAcorn(code, {
+            ecmaVersion: "latest",
+            sourceType: "script",
+            onInsertedSemicolon: /** @type {any} */ (undefined),
+            onTrailingComma: /** @type {any} */ (undefined),
+            allowReturnOutsideFunction: false,
+            allowImportExportEverywhere: true,
+            allowAwaitOutsideFunction: true,
+            allowSuperOutsideMethod: true,
+            allowHashBang: true,
+            checkPrivateFields: false,
+          })
+        ),
+        base,
+        context,
+      },
+    };
+  } catch (error) {
+    if (
+      typeof error === "object" &&
+      error !== null &&
+      "message" in error &&
+      typeof error.message === "string" &&
+      error instanceof SyntaxError
+    ) {
+      return {
+        type: "failure",
+        data: error.message,
+      };
+    } else {
+      throw error;
+    }
+  }
+};
+
+/**
  * @type {(
  *   node: unknown,
  * ) => estree.ScriptProgram}
  */
-const _sanitizeBabel = (root) => {
+const sanitizeBabel = (root) => {
   const todo = [root];
   let length = 1;
   while (length > 0) {
@@ -107,60 +160,38 @@ const _sanitizeBabel = (root) => {
  *   string
  * >}
  */
-export const parseLocal = (code, base, context) => {
+export const parseBabelLocal = (code, base, context) => {
   try {
     return {
       type: "success",
       data: {
         kind: "eval",
-        root: /** @type {any} */ (
-          parseAcorn(code, {
-            ecmaVersion: "latest",
-            sourceType: "script",
-            onInsertedSemicolon: undefined,
-            onTrailingComma: undefined,
+        root: sanitizeBabel(
+          parseBabel(code, {
+            allowImportExportEverywhere: false,
+            allowAwaitOutsideFunction: false,
+            // @ts-ignore
+            allowNewTargetOutsideFunction: true,
             allowReturnOutsideFunction: false,
-            allowImportExportEverywhere: true,
-            allowAwaitOutsideFunction: true,
             allowSuperOutsideMethod: true,
-            allowHashBang: true,
-            checkPrivateFields: false,
-          })
+            allowUndeclaredExports: false,
+            attachComment: false,
+            // Error: The `annexB` option can only be set to `false`.
+            annexB: false,
+            createImportExpressions: true,
+            createParenthesizedExpressions: false,
+            errorRecovery: false,
+            plugins: ["estree"],
+            sourceType: "script",
+            strictMode: false,
+            ranges: false,
+            tokens: false,
+          }).program,
         ),
         base,
         context,
       },
     };
-    // return {
-    //   type: "success",
-    //   data: {
-    //     kind: "eval",
-    //     root: sanitizeBabel(
-    //       parseBabel(code, {
-    //         allowImportExportEverywhere: false,
-    //         allowAwaitOutsideFunction: false,
-    //         // @ts-ignore
-    //         allowNewTargetOutsideFunction: true,
-    //         allowReturnOutsideFunction: false,
-    //         allowSuperOutsideMethod: true,
-    //         allowUndeclaredExports: false,
-    //         attachComment: false,
-    //         // Error: The `annexB` option can only be set to `false`.
-    //         annexB: false,
-    //         createImportExpressions: true,
-    //         createParenthesizedExpressions: false,
-    //         errorRecovery: false,
-    //         plugins: ["estree"],
-    //         sourceType: "script",
-    //         strictMode: false,
-    //         ranges: false,
-    //         tokens: false,
-    //       }).program,
-    //     ),
-    //     base,
-    //     context,
-    //   },
-    // };
   } catch (error) {
     if (
       typeof error === "object" &&
@@ -176,5 +207,27 @@ export const parseLocal = (code, base, context) => {
     } else {
       throw error;
     }
+  }
+};
+
+/**
+ * @type {<B>(
+ *   code: string,
+ *   base: B,
+ *   context: import("../../lib/context").InternalLocalContext,
+ * ) => import("./outcome").Outcome<
+ *   import("../../lib/program").InternalLocalEvalProgram<B>,
+ *   string
+ * >}
+ */
+export const parseLocal = (code, base, context) => {
+  const acorn = parseAcornLocal(code, base, context);
+  // We prefer acorn over babel because it is faster respect the estree format.
+  // The estree babel plugin is supposed to make babel produce valid estree
+  //   but the private identifiers and field are still in the babel format.
+  if (acorn.type === "failure" && acorn.data.startsWith("'new.target'")) {
+    return parseBabelLocal(code, base, context);
+  } else {
+    return acorn;
   }
 };
