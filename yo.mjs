fba78afbd4bb78832d80870b19759f50349f3c49
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-04-26 16:04:58 +0200

    fix: no longer discard define prototype property on default constructor

--- yo.mjs
+++ yo.mjs
@@ -1,3 +1,9 @@
-const x = 123;
-
-export { x as default };
+// file:///Users/lachrist/Desktop/workspace/aran/test262/test/language/module-code/verify-dfs.js
+let _ARAN_ESCAPE_exp_foo;
+export { _ARAN_ESCAPE_exp_foo as foo };
+import "./yoyo.mjs";
+{
+  let _ARAN_ESCAPE_var_original_foo = "deadzone";
+  _ARAN_ESCAPE_var_original_foo = 123;
+  _ARAN_ESCAPE_exp_foo = _ARAN_ESCAPE_var_original_foo;
+}
--- yoyo.mjs
+++ yoyo.mjs
@@ -1,1 +1,6 @@
-export const foo = 123;
+// file:///Users/lachrist/Desktop/workspace/aran/test262/test/language/module-code/verify-dfs-a_FIXTURE.js
+import { foo as _ARAN_ESCAPE_imp_$002e$002fverify$002ddfs$002ejs_foo } from "./yo.mjs";
+{
+  _ARAN_ESCAPE_imp_$002e$002fverify$002ddfs$002ejs_foo;
+  void 0;
+}
