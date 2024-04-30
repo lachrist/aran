4a2a5198c20a21c32fe76676926c7648223a491b
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-04-30 10:08:02 +0200

    test: convert parameter when compiling function code

--- test/262/stages/util/function-code.mjs
+++ test/262/stages/util/function-code.mjs
@@ -1,3 +1,5 @@
+const { String } = globalThis;
+
 /**
  * @type {(
  *   args: import("./aran").Value[],
@@ -7,8 +9,9 @@ export const compileFunctionCode = (args) => {
   if (args.length === 0) {
     return "(function anonymous(\n) {\n\n});";
   } else {
-    return `(function anonymous(\n${args.slice(0, -1).join(",")}\n) {\n${
-      args[args.length - 1]
-    }\n});`;
+    return `(function anonymous(\n${args
+      .slice(0, -1)
+      .map(String)
+      .join(",")}\n) {\n${args[args.length - 1]}\n});`;
   }
 };
