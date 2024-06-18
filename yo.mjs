86ce375049b7d6756b79e120f4f83370eee4abff
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-06-18 10:21:10 +0200

    wip: rebuild generator head

--- yo.mjs
+++ yo.mjs
@@ -1,2 +1,30 @@
+// function* g(
+//   ...{
+//     [Symbol("foo")]: {
+//       ["function.arguments"]: param_function_arguments,
+//       ["this"]: param_this,
+//       ["new.target"]: param_new_target,
+//       ["variable1"]: variable1,
+//     } = {
+//       ["function.arguments"]: "FUNCTION_ARGUMENTS",
+//       ["this"]: "THIS",
+//       ["new.target"]: "NEW_TARGET",
+//       ["variable1"]: "VARIABLE1",
+//     },
+//   }
+// ) {
+//   console.log({
+//     param_function_arguments,
+//     param_this,
+//     param_new_target,
+//     variable1,
+//   });
+// }
 
-async function f ({[await 123]: foo}) { return x};
+function* g() {
+  return 123;
+}
+
+const gg = g();
+console.log(gg.next());
+console.log(gg.return());
