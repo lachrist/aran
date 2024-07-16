45fba8d611fd05b52b46ff7af23a97e93c862c32
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-07-16 21:10:13 +0200

    doc: update issue

--- yoyo.mjs
+++ yoyo.mjs
@@ -1,25 +1,3 @@
-console.log(
-  new (class extends Object {
-    constructor() {
-      try {
-        return;
-      } finally {
-        super();
-      }
-    }
-  })(),
-);
-
-// var o = new C();
-// console.log(typeof o);
-// assert.sameValue(typeof o, "object");
-
-{
-  let x = true;
-
-  while (x) {
-    break;
-    x = false;
-    123;
-  }
-}
+import * as self from "./yoyo.mjs";
+console.log({ foo: self.foo });
+export const foo = 123;
