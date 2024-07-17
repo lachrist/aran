dbdc186ea779355a0f49ccf64693a5e260b87c84
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-07-17 15:22:32 +0200

    refactor: provide an expression instead of a cache to pattern visitor
    This is the first step to fix evaluation order of member assignments inside patterns.

--- yo.mjs
+++ yo.mjs
@@ -1,4 +1,19 @@
-(() => {
-  eval("var x = 123;");
-  let x = 123;
-})();
+[
+  source,
+  source - key,
+  source - key - tostring,
+  get,
+  target,
+  target - key,
+  target - key - tostring,
+  set,
+][
+  (source,
+  source - key,
+  source - key - tostring,
+  target,
+  target - key,
+  get,
+  target - key - tostring,
+  set)
+];
