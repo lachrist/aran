8da0e78c7bf62f3f4f6530394d34f7b105a89c53
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-07-22 11:39:33 +0200

    test: mirror code generation made by new Function call

--- test/262/stages/util/function-code.mjs
+++ test/262/stages/util/function-code.mjs
@@ -5,13 +5,7 @@ const { String } = globalThis;
  *   args: unknown[],
  * ) => string}
  */
-export const compileFunctionCode = (args) => {
-  if (args.length === 0) {
-    return "(function anonymous(\n) {\n\n});";
-  } else {
-    return `(function anonymous(\n${args
-      .slice(0, -1)
-      .map(String)
-      .join(",")}\n) {\n${args[args.length - 1]}\n});`;
-  }
-};
+export const compileFunctionCode = (args) =>
+  `(function anonymous(${args.slice(0, -1).map(String).join(",")}\n) {\n${
+    args[args.length - 1]
+  }\n});`;
