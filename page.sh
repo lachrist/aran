b8b4e5bd673c99e218fa651fb27f97d0b520d52b
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-08-29 12:03:15 +0200

    build: automate github page deployment

---
+++ page.sh
@@ -0,0 +1,15 @@
+mkdir -p page
+echo "disable jekyll" > page/.nojekyll
+
+npx typedoc
+
+npx rollup -c demo/rollup.config.js
+cp demo/index.html page/demo
+
+git branch -D page
+git checkout --orphan page
+git reset
+git add -f page
+git commit -m "deploy to GitHub pages"
+git push -f origin page
+git checkout -f main
