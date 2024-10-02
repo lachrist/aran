1c879b02949fef01ead51cb8ade96fec673c850b
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-10-02 15:23:26 +0200

    wip: update estree-sentry and adjust brand and guard

--- lib/unbuild/estree.d.ts
+++ lib/unbuild/estree.d.ts
@@ -1,4 +1,8 @@
-import type { ObjectProperty } from "estree-sentry";
+import type {
+  ObjectProperty,
+  PropertyDefinition,
+  StaticBlock,
+} from "estree-sentry";
 
 export type ProtoObjectProperty<X> = ObjectProperty<X> & {
   computed: false;
@@ -11,4 +15,14 @@ export type ProtoObjectProperty<X> = ObjectProperty<X> & {
         type: "Literal";
         value: "__proto__";
       };
+};
+
+export type ClassPropertyDefinition<X> = PropertyDefinition<X> & {
+  static: true;
+};
+
+export type StaticPrelude<X> = StaticBlock<X> | ClassPropertyDefinition<X>;
+
+export type InstancePropertyDefinition<X> = PropertyDefinition<X> & {
+  static: false;
 };
