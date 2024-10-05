b2e70127cb2513c5ebc038f07a84a4cbe1e5b37c
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-10-05 08:05:41 +0200

    fix: use correct node hash in class key mapping

---
+++ yo.mjs
@@ -0,0 +1,20 @@
+function* g() {
+  yield 0;
+  yield 1;
+  yield 2;
+}
+
+class TestIterator extends Iterator {
+  get next() {
+    let n = g();
+    return function () {
+      return n.next();
+    };
+  }
+  return() {
+    throw new Test262Error();
+  }
+}
+
+const x = new TestIterator();
+x.drop(0).next();
