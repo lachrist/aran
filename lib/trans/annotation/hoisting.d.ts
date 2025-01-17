97c57cd1bcc867a4aa9a54e9cd55c3b131fe4bff
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-01-17 21:14:26 +0100

    wip: more raw hoisting annotation
    Instead of hidding kinds and trying to compute in advance relevant properties,
    Let's only hoist kinds and let consumer figure properties for themselves.

--- lib/trans/annotation/hoisting.d.ts
+++ lib/trans/annotation/hoisting.d.ts
@@ -1,34 +1,85 @@
 import type { VariableName } from "estree-sentry";
 import type { Hash } from "../hash";
+import type { Tree } from "../../util/tree";
 
-export type SloppyFunction = "nope" | "near" | "away" | "both";
+export type Scoping = {
+  [key in Kind]?: boolean;
+};
 
-export type Write = "perform" | "report" | "ignore";
-
-export type Baseline = "live" | "dead";
-
-export type Initial =
-  | "undefined"
-  | "deadzone"
+export type Kind =
   | "import"
-  | "self-function"
-  | "self-class"
-  | "arguments";
+  | "var"
+  | "let"
+  | "const"
+  | "class"
+  | "class-self"
+  | "arguments"
+  | "function-self-strict"
+  | "function-self-sloppy"
+  | "function-strict"
+  | "function-sloppy-near"
+  | "function-sloppy-away"
+  | "error-complex"
+  | "error-simple"
+  | "param-complex"
+  | "param-simple";
 
-export type Binding = {
+export type Clash = "ignore" | "report" | "remove";
+
+export type FreeBinding = {
+  type: "free";
+  kind: Kind;
   variable: VariableName;
-  duplicable: boolean;
-  initial: Initial;
-  write: Write;
-  sloppy_function_near: number;
-  sloppy_function_away: number;
+  origin: Hash;
+  bind: null;
+};
+
+export type LockBinding = {
+  type: "lock";
+  kind: Kind;
+  variable: VariableName;
+  origin: Hash;
+  bind: Hash;
+};
+
+export type FlagBinding = {
+  type: "flag";
+  kind: Kind;
+  variable: VariableName;
+  origin: Hash;
+  bind: "duplicate" | "keyword";
+};
+
+export type VoidBinding = {
+  type: "void";
+  kind: Kind;
+  variable: VariableName;
+  origin: Hash;
+  bind: null;
+};
+
+export type TodoBinding = FreeBinding;
+
+export type DoneBinding = LockBinding | FlagBinding | VoidBinding;
+
+export type Binding = TodoBinding | DoneBinding;
+
+export type BindingItem = {
+  todo: Tree<TodoBinding>;
+  done: Tree<DoneBinding>;
+};
+
+export type BindingTree = Tree<BindingItem>;
+
+export type Frame = {
+  [key in VariableName]?: Kind[];
+};
+
+export type Hoisting = {
+  [key in Hash]?: Frame;
 };
 
 export type Error = {
   message: string;
   origin: Hash;
-};
-
-export type Hoisting = {
-  [key in Hash]?: Binding[];
 };
