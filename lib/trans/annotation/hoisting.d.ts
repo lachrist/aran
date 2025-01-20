b969ac5515140981b976ba65a058d2d590ad675d
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-01-20 12:44:42 +0100

    refactor: add precise scoping
    Not actually used in the code.
    But might be used in the future.
    Might help understanding hoisting.

--- lib/trans/annotation/hoisting.d.ts
+++ lib/trans/annotation/hoisting.d.ts
@@ -2,10 +2,6 @@ import type { VariableName } from "estree-sentry";
 import type { Hash } from "../hash";
 import type { Tree } from "../../util/tree";
 
-export type Scoping = {
-  [key in Kind]?: boolean;
-};
-
 export type Kind =
   | "import"
   | "var"
@@ -23,6 +19,123 @@ export type Kind =
   | "error-simple"
   | "param-complex"
   | "param-simple";
+
+export type PreciseScoping<K extends Kind> = {
+  [key in Kind]: key extends K ? true : false | null;
+};
+
+export type Scoping = { [key in Kind]: boolean | null };
+
+export type UnboundModuleKind = never;
+
+export type UnboundScriptKind = Kind &
+  (
+    | "var"
+    | "let"
+    | "const"
+    | "class"
+    | "function-strict"
+    | "function-sloppy-near"
+    | "function-sloppy-away"
+  );
+
+export type UnboundStrictEvalKind = never;
+
+export type UnboundSloppyEvalKind =
+  | "var"
+  | "sloppy-function-near"
+  | "sloppy-function-away";
+
+export type ModuleKind = Kind &
+  ("import" | "var" | "let" | "const" | "class" | "function-strict");
+
+export type ScriptKind = Kind & never;
+
+export type SloppyEvalKind = Kind & ("let" | "const" | "class");
+
+export type StrictEvalKind = Kind &
+  ("var" | "let" | "const" | "class" | "function-strict");
+
+export type ScriptGlobalKind = Kind &
+  (
+    | "var"
+    | "let"
+    | "const"
+    | "class"
+    | "function-strict"
+    | "function-sloppy-near"
+    | "function-sloppy-away"
+  );
+
+export type SloppyEvalGlobalKind = Kind & ("var" | "function-sloppy-away");
+
+export type StrictEvalGlobalKind = Kind & never;
+
+export type ProgramKind = Kind &
+  (
+    | "import"
+    | "var"
+    | "let"
+    | "const"
+    | "class"
+    | "function-strict"
+    | "function-sloppy-near"
+    | "function-sloppy-away"
+  );
+
+export type ComplexClosureHeadKind = Kind &
+  (
+    | "function-self-strict"
+    | "function-self-sloppy"
+    | "arguments"
+    | "param-complex"
+  );
+
+export type ComplexClosureBodyKind = Kind &
+  (
+    | "let"
+    | "const"
+    | "class"
+    | "var"
+    | "function-strict"
+    | "function-sloppy-near"
+    | "function-sloppy-away"
+  );
+
+export type SimpleClosureKind = Kind &
+  (
+    | "let"
+    | "const"
+    | "class"
+    | "var"
+    | "function-strict"
+    | "function-sloppy-near"
+    | "function-sloppy-away"
+    | "function-self-strict"
+    | "function-self-sloppy"
+    | "arguments"
+    | "param-simple"
+  );
+
+export type IfBranchKind = Kind & "function-sloppy-near";
+
+export type BlockKind = Kind &
+  ("let" | "const" | "class" | "function-strict" | "function-sloppy-near");
+
+export type ClassKind = Kind & "class-self";
+
+export type StaticBlockKind = Kind &
+  (
+    | "var"
+    | "let"
+    | "const"
+    | "function-strict"
+    | "function-sloppy-near"
+    | "function-sloppy-away"
+    | "class"
+  );
+
+export type CatchKind = Kind & ("error-complex" | "error-simple");
 
 export type Clash = "ignore" | "report" | "remove";
 
