1ff844f2b203c44b361c7e159beea83aaec9820a
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-01-21 16:31:01 +0100

    fix: detect conflict for let x; eval('var x;');

--- lib/trans/prelude/external.d.ts
+++ lib/trans/prelude/external.d.ts
@@ -3,6 +3,7 @@ import type { Hash } from "../hash";
 import type { RootKind } from "../scope/variable/root";
 
 export type ReifyExternal = {
+  dynamic: boolean;
   kinds: RootKind[];
   variable: VariableName;
   origin: Hash;
