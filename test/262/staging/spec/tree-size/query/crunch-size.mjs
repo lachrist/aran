f17b1fd99466a355424a9071106f3dd8df7abb31
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-02-07 12:10:22 +0100

    test: plot tree-size

--- test/262/staging/spec/tree-size/query/crunch-size.mjs
+++ test/262/staging/spec/tree-size/query/crunch-size.mjs
@@ -1,35 +1,66 @@
-import { readFile, writeFile } from "node:fs/promises";
+import { open, writeFile } from "node:fs/promises";
 import { AranExecError } from "../../../../error.mjs";
-import { isNotEmptyString } from "../../../../util/string.mjs";
+import { createInterface } from "node:readline/promises";
 
-const { Object, Math, URL, JSON, Array } = globalThis;
+const { Object, Math, URL, JSON, Array, Infinity, Symbol } = globalThis;
+
+//////////
+// Util //
+//////////
 
 /**
  * @type {(
- *   value: unknown,
- * ) => value is 0}
+ *    handle: import("node:fs/promises").FileHandle,
+ * ) => AsyncIterator<string>}
  */
-const isZero = (value) => value === 0;
+const createIterable = (handle) =>
+  createInterface({
+    input: handle.createReadStream({ encoding: "utf-8" }),
+    crlfDelay: Infinity,
+  })[Symbol.asyncIterator]();
 
 /**
  * @type {(
- *   array: unknown[],
+ *   num1: number,
+ *   num2: number,
+ * ) => number}
+ */
+const add = (num1, num2) => num1 + num2;
+
+/**
+ * @type {(
+ *   numbers: number[],
+ * ) => number}
+ */
+const sum = (numbers) => numbers.reduce(add, 0);
+
+/**
+ * @type {(
+ *   table: number[][],
+ * ) => number[]}
+ */
+const sumEach = (table) => table.map(sum);
+
+/**
+ * @type {<X>(
+ *   value: X | null,
+ * ) => value is X}
+ */
+const isNotNull = (value) => value !== null;
+
+/**
+ * @type {(
+ *   string: string,
  * ) => boolean}
  */
-const isRepeatArray = (array) => {
-  if (array.length === 0) {
-    return true;
-  } else {
-    const base = array[0];
-    const { length } = array;
-    for (let index = 1; index < length; index++) {
-      if (array[index] !== base) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const isNotEmptyString = (string) => string !== "";
+
+/**
+ * @type {<X>(
+ *   table: X[][],
+ * ) => X[]}
+ */
+const flaten = (table) => table.flat(1);
 
 /**
  * @type {(
@@ -58,116 +89,50 @@ const getCommonLength = (arrays) => {
 
 /**
  * @type {(
- *   divident: number,
- *   divisor: number,
+ *   val1: number,
+ *   val2: number,
  * ) => number}
  */
-const computeRatio = (divident, divisor) => {
-  if (divident === 0 && divisor === 0) {
+const computePercentIncrease = (val1, val2) => {
+  if (val1 === 0 && val2 === 0) {
     return 0;
   }
-  if (divident === 0) {
-    throw new AranExecError("divident is zero", { divident, divisor });
+  if (val1 === 0) {
+    throw new AranExecError("divident is zero", { val1, val2 });
   }
-  if (divisor === 0) {
-    throw new AranExecError("divisor is zero", { divident, divisor });
+  if (val2 === 0) {
+    throw new AranExecError("divisor is zero", { val1, val2 });
   }
-  return Math.round((10000 * divident) / divisor) / 10000;
+  return Math.round((100 * (val2 - val1)) / val1);
 };
+
+/**
+ * @type {(
+ *   nums1: number[],
+ *   nums2: number[],
+ * ) => number[]}
+ */
+const computeEachPercentIncrease = (nums1, nums2) => {
+  const length = getCommonLength([nums1, nums2]);
+  const result = [];
+  for (let index = 0; index < length; index++) {
+    result.push(computePercentIncrease(nums1[index], nums2[index]));
+  }
+  return result;
+};
+
+//////////
+// Type //
+//////////
 
 /**
  * @typedef {number & { __brand: "Hash" }} Hash
  * @typedef {number & { __brand: "Size" }} Size
- * @typedef {number & { __brand: "BranchIndex" }} BranchIndex
- * @typedef {Record<BranchIndex, Size | Hash> & { length: number }} Test
- * @typedef {import("../../../../test-case").TestIndex} TestIndex
- * @typedef {Record<TestIndex, Test> & { length: number }} Suite
+ * @typedef {Size[]} Test
+ * @typedef {Test[]} Suite
  * @typedef {"intra" | "inter"} SuiteName
  * @typedef {Record<SuiteName, Suite>} SuiteRecord
  */
-
-////////////
-// Access //
-////////////
-
-/**
- * @type {{[key in SuiteName]: null}}
- */
-const stage_name_record = {
-  intra: null,
-  inter: null,
-};
-
-const stage_name_enum = /** @type {SuiteName[]} */ (
-  Object.keys(stage_name_record)
-);
-
-/**
- * @type {(
- *   test: Test,
- *   index: BranchIndex,
- * ) => Size}
- */
-const getTestSize = (test, index) =>
-  /** @type {Size} */ (test[/** @type {BranchIndex} */ (2 * index)]);
-
-/**
- * @type {(
- *   test: Test,
- *   index: BranchIndex,
- * ) => Hash}
- */
-const getTestHash = (test, index) =>
-  /** @type {Hash} */ (test[/** @type {BranchIndex} */ (2 * index + 1)]);
-
-/**
- * @type {(
- *   suite: Suite,
- *   test_index: TestIndex,
- * ) => Test}
- */
-const getSuiteTest = (suite, test_index) => suite[test_index];
-
-/**
- * @type {(
- *   suites: Suite[],
- * ) => Iterable<TestIndex>}
- */
-const iterateTestIndex = function* (suites) {
-  const suite_length = getCommonLength(suites);
-  for (let test_index = 0; test_index < suite_length; test_index++) {
-    yield /** @type {TestIndex} */ (test_index);
-  }
-};
-
-/**
- * @type {(
- *   tests: Test[],
- * ) => Iterable<BranchIndex>}
- */
-const iterateBranchIndex = function* (tests) {
-  const double_test_length = getCommonLength(tests);
-  if (double_test_length % 2 !== 0) {
-    throw new AranExecError("odd double test length", { double_test_length });
-  }
-  const test_length = double_test_length / 2;
-  for (let branch_index = 0; branch_index < test_length; branch_index++) {
-    yield /** @type {BranchIndex} */ (branch_index);
-  }
-};
-
-/**
- * @type {(
- *   test: Test,
- * ) => Size}
- */
-const sumTest = (test) => {
-  let total = 0;
-  for (const branch_index of iterateBranchIndex([test])) {
-    total += getTestSize(test, branch_index);
-  }
-  return /** @type {Size} */ (total);
-};
 
 //////////
 // Load //
@@ -175,33 +140,42 @@ const sumTest = (test) => {
 
 /**
  * @type {(
- *   suites: SuiteRecord,
+ *   line: string,
+ * ) => [Size[], Hash[]]}
+ */
+const parseLine = (line) => {
+  const data = JSON.parse(line);
+  if (!Array.isArray(data)) {
+    throw new AranExecError("not an array", { data });
+  }
+  if (data.length % 2 !== 0) {
+    throw new AranExecError("odd length", { data });
+  }
+  const sizes = [];
+  const hashes = [];
+  for (let index = 0; index < data.length; index += 2) {
+    sizes.push(data[index]);
+    hashes.push(data[index + 1]);
+  }
+  return [sizes, hashes];
+};
+
+/**
+ * @type {(
+ *   hashing: Hash[][],
+ *   location: number,
  * ) => void}
  */
-const verifySuiteRecord = (suite_record) => {
-  for (const name of stage_name_enum) {
-    if (!Object.hasOwn(suite_record, name)) {
-      throw new AranExecError("missing suite", { name });
-    }
-  }
-  const suites = Object.values(suite_record);
-  for (const test_index of iterateTestIndex(suites)) {
-    const tests = suites.map((suite) => getSuiteTest(suite, test_index));
-    for (const branch_index of iterateBranchIndex(tests)) {
-      const hashes = tests.map((test) => getTestHash(test, branch_index));
-      if (!isRepeatArray(hashes)) {
-        throw new AranExecError("hash mismatch", {
+const verifyHashing = (hashing, location) => {
+  const length = getCommonLength(hashing);
+  for (let index = 0; index < length; index++) {
+    const base = hashing[0][index];
+    for (const hashes of hashing) {
+      if (hashes[index] !== base) {
+        throw new AranExecError("mismatched hashing", {
+          location,
+          index,
           hashes,
-          test_index,
-          branch_index,
-        });
-      }
-      const sizes = tests.map((test) => getTestSize(test, branch_index));
-      if (sizes.some(isZero) && !sizes.every(isZero)) {
-        throw new AranExecError("zero mixture", {
-          sizes,
-          test_index,
-          branch_index,
         });
       }
     }
@@ -210,33 +184,76 @@ const verifySuiteRecord = (suite_record) => {
 
 /**
  * @type {(
- *   line: string,
- * ) => Test}
+ *   urls: URL[],
+ * ) => Promise<Suite[]>}
  */
-const parseTest = (line) => {
-  if (line === "") {
-    return [];
-  } else {
-    const data = JSON.parse(line);
-    if (!Array.isArray(data)) {
-      throw new AranExecError("not an array", { data });
+const loadSuiteRecord = async (urls) => {
+  const handles = [];
+  try {
+    for (const url of urls) {
+      handles.push(await open(url, "r"));
     }
-    for (const item of data) {
-      if (typeof item !== "number") {
-        throw new AranExecError("not a number", { data, item });
+    const iterators = handles.map(createIterable);
+    /**
+     * @type {Suite[]}
+     */
+    const suites = Array.from({ length: iterators.length }, () => []);
+    let index = 0;
+    while (true) {
+      /**
+       * @type {(string | null)[]}
+       */
+      const lines = [];
+      for (const iterator of iterators) {
+        const { value, done } = await iterator.next();
+        lines.push(done ? null : value);
       }
+      if (lines.every(isNotNull)) {
+        if (lines.every(isNotEmptyString)) {
+          let index = 0;
+          const hashing = [];
+          for (const [sizes, hashes] of lines.map(parseLine)) {
+            suites[index].push(sizes);
+            hashing.push(hashes);
+            index++;
+          }
+          verifyHashing(hashing, index);
+        } else {
+          if (lines.some(isNotEmptyString)) {
+            throw new AranExecError("mismatched exclusion", { index, lines });
+          }
+        }
+      } else {
+        if (lines.some(isNotNull)) {
+          throw new AranExecError("mismatched suite length", { index, lines });
+        }
+        break;
+      }
+      index++;
     }
-    return data;
+    return suites;
+  } finally {
+    for (const handle of handles) {
+      await handle.close();
+    }
   }
 };
 
+//////////
+// Main //
+//////////
+
 /**
- * @type {(
- *   content: string,
- * ) => Suite}
+ * @type {{[key in SuiteName]: null}}
  */
-const parseSuite = (content) =>
-  content.split("\n").filter(isNotEmptyString).map(parseTest);
+const suite_name_record = {
+  intra: null,
+  inter: null,
+};
+
+const suite_name_enum = /** @type {SuiteName[]} */ (
+  Object.keys(suite_name_record)
+);
 
 /**
  * @type {(
@@ -246,122 +263,34 @@ const parseSuite = (content) =>
 const locateStageOutput = (procedural) =>
   new URL(`../basic/stage-${procedural}-output.jsonl`, import.meta.url);
 
-/**
- * @type {() => Promise<SuiteRecord>}
- */
-const loadSuiteRecord = async () => ({
-  intra: parseSuite(await readFile(locateStageOutput("intra"), "utf-8")),
-  inter: parseSuite(await readFile(locateStageOutput("inter"), "utf-8")),
-});
-
-///////////////
-// Aggregate //
-///////////////
-
-/**
- * @type {(
- *   suite: Suite,
- * ) => Size[]}
- */
-const aggregateBranch = (suite) => {
-  /** @type {Size[]} */
-  const sizes = [];
-  for (const test_index of iterateTestIndex([suite])) {
-    const test = getSuiteTest(suite, test_index);
-    for (const branch_index of iterateBranchIndex([test])) {
-      sizes.push(getTestSize(test, branch_index));
-    }
-  }
-  return sizes;
-};
-
-/**
- * @type {(
- *   suite: Suite,
- * ) => Size[]}
- */
-const aggregateTest = (suite) => {
-  /** @type {Size[]} */
-  const sizes = [];
-  for (const test_index of iterateTestIndex([suite])) {
-    const test = getSuiteTest(suite, test_index);
-    sizes.push(sumTest(test));
-  }
-  return sizes;
-};
-
-///////////
-// Ratio //
-///////////
-
-/**
- * @type {(
- *   suite1: Suite,
- *   suite2: Suite,
- * ) => number[]}
- */
-const ratioBranch = (suite1, suite2) => {
-  /** @type {number[]} */
-  const ratios = [];
-  for (const test_index of iterateTestIndex([suite1, suite2])) {
-    const test1 = getSuiteTest(suite1, test_index);
-    const test2 = getSuiteTest(suite2, test_index);
-    for (const branch_index of iterateBranchIndex([test1, test2])) {
-      ratios.push(
-        computeRatio(
-          getTestSize(test1, branch_index),
-          getTestSize(test2, branch_index),
-        ),
-      );
-    }
-  }
-  return ratios;
-};
-
-/**
- * @type {(
- *   suite1: Suite,
- *   suite2: Suite,
- * ) => number[]}
- */
-const ratioTest = (suite1, suite2) => {
-  const ratios = [];
-  const suites = [suite1, suite2];
-  for (const test_index of iterateTestIndex(suites)) {
-    const sum1 = sumTest(getSuiteTest(suite1, test_index));
-    const sum2 = sumTest(getSuiteTest(suite2, test_index));
-    ratios.push(computeRatio(sum1, sum2));
-  }
-  return ratios;
-};
-
-//////////
-// Main //
-//////////
-
 const main = async () => {
-  const suites = await loadSuiteRecord();
-  verifySuiteRecord(suites);
-  for (const name of stage_name_enum) {
-    await writeFile(
-      new URL(`../output/${name}-aggregate-branch.txt`, import.meta.url),
-      aggregateBranch(suites[name]).join("\n") + "\n",
-      "utf8",
-    );
-    await writeFile(
-      new URL(`../output/${name}-aggregate-test.txt`, import.meta.url),
-      aggregateTest(suites[name]).join("\n") + "\n",
-      "utf8",
-    );
+  const suites = await loadSuiteRecord(suite_name_enum.map(locateStageOutput));
+  const flatening = suites.map(flaten);
+  const aggregate = suites.map(sumEach);
+  {
+    let index = 0;
+    for (const name of suite_name_enum) {
+      await writeFile(
+        new URL(`../output/${name}-flat.txt`, import.meta.url),
+        flatening[index].join("\n") + "\n",
+        "utf8",
+      );
+      await writeFile(
+        new URL(`../output/${name}-aggr.txt`, import.meta.url),
+        aggregate[index].join("\n") + "\n",
+        "utf8",
+      );
+      index++;
+    }
   }
   await writeFile(
-    new URL("../output/ratio-branch.txt", import.meta.url),
-    ratioBranch(suites.intra, suites.inter).join("\n") + "\n",
+    new URL("../output/ratio-flat.txt", import.meta.url),
+    computeEachPercentIncrease(flatening[0], flatening[1]).join("\n") + "\n",
     "utf8",
   );
   await writeFile(
-    new URL("../output/ratio-test.txt", import.meta.url),
-    ratioTest(suites.intra, suites.inter).join("\n") + "\n",
+    new URL("../output/ratio-aggr.txt", import.meta.url),
+    computeEachPercentIncrease(aggregate[0], aggregate[1]).join("\n") + "\n",
     "utf8",
   );
 };
---
+++ test/262/staging/spec/tree-size/query/plot-size.py
@@ -0,0 +1,53 @@
+import numpy
+import os
+import matplotlib.pyplot as plot
+
+def locate (name):
+  return os.path.abspath(
+    os.path.join(
+      __file__,
+      "..",
+      "..",
+      "output",
+      name,
+    ),
+  );
+
+def load (path):
+  file = open(path)
+  try:
+    return list(map(int, filter(str.strip, file)))
+  finally:
+    file.close();
+
+def filterOutliner (data):
+  max = numpy.percentile(data, 97.5)
+  return list(filter(lambda x: x <= max, data))
+
+def plotBar(name):
+  try:
+    data = filterOutliner(load(locate(name + ".txt")))
+    plot.hist(
+      data,
+      bins = min(100, max(data))
+    );
+    plot.grid(axis="y", linestyle="--")
+    plot.savefig(locate(name + ".pdf"));
+  finally:
+    plot.close()
+
+names = [
+  "inter-aggr",
+  "inter-flat",
+  "intra-aggr",
+  "intra-flat",
+  "ratio-aggr",
+  "ratio-flat",
+]
+
+def main ():
+  for name in names:
+    plotBar(name)
+
+if __name__ == "__main__":
+  main()
