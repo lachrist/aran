b6c5f3a4e70ad634169c750e7ff1abe1fd41e9a4
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-03-04 17:49:38 +0100

    fix: demo deploy

--- page.sh
+++ page.sh
@@ -1,12 +1,13 @@
 mkdir -p page
+mkdir -p page/demo
 echo "disable jekyll" > page/.nojekyll
 
 npx typedoc
 
 node demo/bundle.mjs
-cp demo/case/apply.html page/demo
-cp demo/case/trace.html page/demo
-cp demo/case/provenance.html page/demo
+cp demo/case/apply.html page/demo/apply.html
+cp demo/case/trace.html page/demo/trace.html
+cp demo/case/provenance.html page/demo/provenance.html
 
 git branch -D page
 git checkout --orphan page
