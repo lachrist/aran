12a6885f52f4185a755c401976817fb4be229bd4
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-03-08 10:32:45 +0100

    doc: prod ready

--- doc/build.sh
+++ doc/build.sh
@@ -1,8 +1,8 @@
-mv doc/out/demos/worker.mjs doc/worker.mjs
+# mv doc/out/demos/worker.mjs doc/worker.mjs
 node doc/demo/markdown.mjs
 bundle exec jekyll build  --config doc/src/_config.yml,doc/src/_config.$1.yml
 node doc/demo/bundle.mjs
 npx typedoc --options doc/typedoc.json
 cp -r doc/assets/* doc/out/assets/
 echo "disable jekyll" > doc/out/.nojekyll
-mv doc/worker.mjs doc/out/demos/worker.mjs
+# mv doc/worker.mjs doc/out/demos/worker.mjs
